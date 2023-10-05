d36e0c25da66a3feb0ce18ef22eae751a21d2521
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-10-05 12:35:36 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,37 +1,40 @@
 from graphviz import Digraph
 
-dot = Digraph(comment='The JSMC Production Process')
+dot = Digraph(comment='The JSMC Production Process', format='png')
+dot.attr(rankdir='TB', size='10,10')
 
-# Main Process Nodes
-dot.node('A', 'Pre-Production')
-dot.node('B', 'Initial Preparation')
-dot.node('C', 'Fabrication')
-dot.node('D', 'Assembly Line')
-dot.node('E', 'Testing Phase')
-dot.node('F', 'Final Delivery')
+# Main Process Nodes with different shapes and colors
+dot.node('A', 'Pre-Production', shape='ellipse', color='black', style='filled', fillcolor='lightyellow')
+dot.node('B', 'Initial Preparation', shape='box', color='black', style='filled', fillcolor='lightpink')
+dot.node('C', 'Fabrication', shape='box', color='black', style='filled', fillcolor='lightcyan')
+dot.node('D', 'Assembly Line', shape='box', color='black', style='filled', fillcolor='lightcoral')
+dot.node('E', 'Testing Phase', shape='ellipse', color='black', style='filled', fillcolor='lightgreen')
+dot.node('F', 'Final Delivery', shape='ellipse', color='black', style='filled', fillcolor='lightgray')
 
 dot.edges(['AB', 'BC', 'CD', 'DE', 'EF'])
 
-# Sub-nodes for Initial Preparation
-dot.node('BA', 'Shot Blasting')
-dot.node('BB', 'Painting and De-Masking')
-dot.edges(['BBA', 'ABB'])  # Connect sub-nodes to main node B
+# Initial Preparation Sub-Nodes
+dot.node('BA', 'Shot Blasting\n- Rust removal from hulls & turrets', shape='parallelogram', color='black')
+dot.node('BB', 'Painting & De-Masking', shape='parallelogram', color='black')
+dot.edges([('B', 'BA'), ('B', 'BB')])
 
-# Sub-nodes for Fabrication
-dot.node('CA', 'Cutting Operations')
-dot.node('CB', 'Welding Operations')
-dot.edges(['CCA', 'BCC'])  # Connect sub-nodes to main node C
+# Fabrication Sub-Nodes
+dot.node('CA', 'Cutting Operations\n- Laser, plasma, water jet cutting\n- Titanium alloy sheet cutting', shape='rectangle', color='black')
+dot.node('CB', 'Welding Operations\n- Robotic & friction stir welding\n- Vehicle component fabrication', shape='rectangle', color='black')
+dot.edges([('C', 'CA'), ('C', 'CB')])
 
-# Sub-nodes for Assembly Line
-dot.node('DA', 'Hull Assembly')
-dot.node('DB', 'Turret Assembly')
-dot.node('DC', 'Final Assembly')
-dot.edges(['DDA', 'CDD', 'DDB', 'DDC'])  # Connect sub-nodes to main node D
+# Assembly Line Sub-Nodes
+dot.node('DA', 'Hull Assembly\n- Internal equipment & systems installation\n- Suspension components installation', shape='rectangle', color='black')
+dot.node('DB', 'Turret Assembly\n- Cannon mounting & recoil testing\n- Ammo doors & component addition', shape='rectangle', color='black')
+dot.edges([('D', 'DA'), ('D', 'DB')])
+dot.node('DC', 'Final Assembly\n- Turret & hull marriage\n- Armor, skirting, final adjustments', shape='rectangle', color='black')
+dot.edges([('DA', 'DC'), ('DB', 'DC')])
 
-# Sub-nodes for Testing Phase
-dot.node('EA', 'QA & Inspection')
-dot.node('EB', 'Road Testing')
-dot.node('EC', 'Final Acceptance Testing')
-dot.edges(['EEA', 'DEE', 'EEB', 'EEC'])  # Connect sub-nodes to main node E
+# Testing Phase Sub-Nodes
+dot.node('EA', 'QA & Inspection\n- Multiple component inspections\n- Fire control systems calibration', shape='rectangle', color='black', style='filled', fillcolor='white')
+dot.node('EB', 'Road Testing\n- Evaluation track road tests\n- Performance & tolerance tests', shape='rectangle', color='black', style='filled', fillcolor='white')
+dot.node('EC', 'Final Acceptance Testing\n- GDLS & DCMA inspection\n- 1,200 item inspection process', shape='rectangle', color='black', style='filled', fillcolor='white')
+dot.edges([('E', 'EA'), ('E', 'EB'), ('E', 'EC')])
 
-dot.render('jsmc_process.gv', view=True)  # Output: jsmc_process.gv.pdf
+# Render the Graph
+dot.render('jsmc_process', view=True)
