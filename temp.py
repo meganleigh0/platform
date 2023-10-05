d809a965b6d3f8aaa3e0c5ca94e8905a1d7a961e
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-10-05 13:38:14 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,87 +1,78 @@
-digraph G {
-    graph [rankdir=TB, size="10,10"]
+## Simulation Model Design for JSMC Manufacturing Process
 
-    // Pre-production and Initial Preparation
-    "Pre-Production" [shape=ellipse, fillcolor=lightyellow, style=filled];
-    "Initial Preparation" [shape=ellipse, fillcolor=lightpink, style=filled];
-    "Hull & Turret Divorce" [shape=box, label="Hull & Turret Divorce\n- Separation of Hull & Turret"];
-    
-    "Pre-Production" -> "Initial Preparation";
-    "Initial Preparation" -> "Hull & Turret Divorce";
+### I. System Description:
+  - **Simulation Scope:** From Hull & Turret Divorce to Final Delivery
+  - **System Components:** Pre-Production, Plant 1 Operations, Plant 3 Assembly Line, Testing Phase, Final Delivery
 
-    subgraph cluster_0 {
-        label = "Plant 1: Initial Operations & Fabrication";
-        color = lightgrey;
-        
-        // Hull Operations in Plant 1
-        "Hull Tear Down" [shape=box, label="Hull Tear Down\n- Divorce of Hull from Turret"];
-        "Hull Appurtenance" [shape=box, label="Hull Appurtenance\n- Prepping Hull for Assembly Line"];
-        "Hull Paint" [shape=box, label="Hull Shot Blast Painting\n- Rust removal from Hull"];
-        
-        "Hull & Turret Divorce" -> "Hull Tear Down";
-        "Hull Tear Down" -> "Hull Appurtenance";
-        "Hull Appurtenance" -> "Hull Paint";
+### II. Entities:
+  - **Units:**
+    - Attributes: Type (Hull/Turret), Age, Status
+  - **Parts:**
+    - Attributes: Type, Quantity Available, Lead Time, Reorder Point
+  - **Stations:**
+    - Attributes: Type, Capacity, Assembly Requirement
 
-        // Turret Operations in Plant 1
-        "Remove Turr Rails and Appurt" [shape=box, label="Remove Turret Rails & Appurtenances\n- Preparation for Modifications"];
-        "Turr Machine" [shape=box, label="Turret Machining\n- Prepping Turret for Armor Attachment"];
-        "Turr Armor" [shape=box, label="Turret Armor Attachment\n- Adding Protective Armor"];
-        "Turr Appurt" [shape=box, label="Turret Appurtenances Attachment\n- Adding necessary components"];
-        "Turr Paint" [shape=box, label="Turret Shot Blast Painting\n- Rust removal from Turret"];
-        
-        "Hull & Turret Divorce" -> "Remove Turr Rails and Appurt";
-        "Remove Turr Rails and Appurt" -> "Turr Machine";
-        "Turr Machine" -> "Turr Armor";
-        "Turr Armor" -> "Turr Appurt";
-        "Turr Appurt" -> "Turr Paint";
+### III. Processes:
+  - **Pre-Production:**
+    - Hull & Turret Divorce
+  - **Plant 1 Operations:**
+    - Hull Operations: Hull Tear Down, Hull Appurtenance, Hull Paint
+    - Turret Operations: Removal of Turret Rails, Turret Machining, Turret Armor Attachment, Turret Appurtenances Attachment, Turret Painting
+    - Fabrication: Cutting Operations, Welding Operations
+  - **Plant 3 Assembly Line:**
+    - Hull Assembly Line
+    - Turret Assembly Line
+    - Marriage & Final Assembly
+  - **Testing Phase:**
+    - QA & Inspection, Road Testing, Final Acceptance Testing
+  - **Inventory Management:**
+    - Ordering Process
+    - Inventory Update
+  - **Forecasting Process**
 
-        // Fabrication in Plant 1
-        "Fabrication" [shape=ellipse, fillcolor=lightcyan, style=filled];
-        "Cutting Operations" [shape=box, label="Cutting Operations\n- Laser, plasma, water jet cutting\n- Titanium alloy sheet cutting"];
-        "Welding Operations" [shape=box, label="Welding Operations\n- Robotic & friction stir welding\n- Vehicle component fabrication"];
-        
-        "Initial Preparation" -> "Fabrication";
-        "Fabrication" -> "Cutting Operations";
-        "Fabrication" -> "Welding Operations";
-    }
+### IV. Resources:
+  - **Inventory:** Tracked for each part
+  - **Assembly Stations:** With specific capacities and assembly requirements
 
-    subgraph cluster_1 {
-        label = "Plant 3: Assembly Line";
-        color = lightgrey;
-        
-        // Hull Assembly Line
-        "Hull Assembly Line" [shape=box, fillcolor=lightcyan];
-        "Hull Paint" -> "Hull Assembly Line";
-        
-        // Turret Assembly Line
-        "Turr Assembly Line" [shape=box, fillcolor=lightcyan];
-        "Turr Paint" -> "Turr Assembly Line";
-        
-        // Marriage & Final Assembly
-        "Marriage & Final Assembly" [shape=ellipse, fillcolor=lightcoral, style=filled];
-        "Hull Assembly Line" -> "Marriage & Final Assembly";
-        "Turr Assembly Line" -> "Marriage & Final Assembly";
-        "Fabrication" -> "Marriage & Final Assembly"; // Adding this line
-    }
+### V. Events:
+  - **Part Order Placement**
+  - **Part Order Arrival**
 
-    subgraph cluster_2 {
-        label = "Vehicle: Testing to Delivery";
-        color = lightgrey;
-        
-        "Testing Phase" [shape=ellipse, fillcolor=lightgreen, style=filled];
-        "Marriage & Final Assembly" -> "Testing Phase";
-        
-        // Testing Phase Sub-Nodes
-        "QA & Inspection" [shape=box, label="QA & Inspection\n- Component Inspections\n- Fire Control System Calibration"];
-        "Road Testing" [shape=box, label="Road Testing\n- Performance & Tolerance Tests"];
-        "Final Acceptance Testing" [shape=box, label="Final Acceptance Testing\n- Over 1,200 Item Inspection"];
-        "Testing Phase" -> "QA & Inspection";
-        "Testing Phase" -> "Road Testing";
-        "Testing Phase" -> "Final Acceptance Testing";
-        
-        "Final Delivery" [shape=ellipse, fillcolor=lightgray, style=filled];
-        "QA & Inspection" -> "Final Delivery";
-        "Road Testing" -> "Final Delivery";
-        "Final Acceptance Testing" -> "Final Delivery";
-    }
-}
+### VI. Data Inputs:
+  - **Inventory Data:** Initial levels, reorder points, lead times
+  - **Consumption Rates:** Rate of part consumption at each station
+
+### VII. System States:
+  - **Inventory Levels:** Dynamic tracking of each part's inventory
+
+### VIII. Simulation Objectives:
+  - **Planning & Tracking Tool:**
+    - Effective scheduling and tracking of the entire manufacturing process
+  - **Sensitivity/Risk Analysis:**
+    - Analyze risks and sensitivities related to late part arrivals
+  - **Inventory Optimization:**
+    - Minimize holding costs, prevent shortages, optimize reorder points and quantities
+
+### IX. Performance Metrics:
+  - **Operational:**
+    - Production Time
+    - Throughput
+    - Utilization of Stations
+  - **Inventory:**
+    - Holding Costs
+    - Stockout Incidents (and use of Golden LRUs)
+    - Order Placement Frequency
+
+
+### X. Development Plan:
+  - **Step 1:** Implement basic SimPy model
+  - **Step 2:** Integrate Inventory Management
+  - **Step 3:** Develop & integrate Forecasting Module
+  - **Step 4:** Validate and Optimize the simulation model
+  - **Step 5:** Implement GUI (using Flask or an alternative)
+  - **Step 6:** Conduct comprehensive testing and validation
+
+### XI. Implementation Approach:
+  - Develop and validate in incremental steps
+  - Ensure modularity for easy testing and validation of each component
+  - Utilize historical data for validation and improvement of the simulation model
