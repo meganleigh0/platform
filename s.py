25c3e11ff56f0393e1272e86701c941cbd86bba7
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-10-23 11:51:23 -0400

    Update s.py

--- s.py
+++ s.py
@@ -1,15 +1,33 @@
 import pandas as pd
-import plotly.express as px
+import plotly.graph_objects as go
 
+# Data
+data = {
+    'Time': [0.00, 0.00, 1.00, 1.00, 2.00, 2.00],
+    'Interaction': ['Starting assy 1', 'Starting assy 2', 'Completed assy 1', 'Starting assy 1', 'Completed assy 2', 'Started assy 2'],
+    'ID': [101, 102, 101, 101, 102, 102],
+    'Vehicle': [1, 1, 1, 2, 1, 2],
+    'Section': ['p1', 'p1', 'p1', 'p1', 'p1', 'p1']
+}
 
-# Create the line plot
-fig = px.line(df, x='Time', y='ID', color='Vehicle', 
-              line_shape='linear', title='Assembly Interactions over Time')
+# Create a DataFrame
+df = pd.DataFrame(data)
 
-# Add scatter points on top of the lines
-scatter = px.scatter(df, x='Time', y='ID', color='Vehicle')
-for trace in scatter.data:
-    fig.add_trace(trace)
+# Create the figure
+fig = go.Figure()
+
+# Loop through each unique combination of assembly ID and vehicle
+for (ID, vehicle), group in df.groupby(['ID', 'Vehicle']):
+    # Check if we have both start and end points
+    if group.shape[0] == 2:
+        fig.add_trace(go.Scatter(x=group['Time'], y=group['ID'],
+                                 mode='lines+markers',
+                                 name=f"Vehicle {vehicle}, ID {ID}"))
+
+# Adjust layout
+fig.update_layout(title='Assembly Interactions over Time',
+                  xaxis_title='Time',
+                  yaxis_title='Assembly ID')
 
 # Show the figure
 fig.show()
