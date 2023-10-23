83146b3dc2c722d3478b2a5f168c4a6fc86dd63a
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-10-23 11:16:59 -0400

    Update s.py

--- s.py
+++ s.py
@@ -1,67 +1,33 @@
-def assign_station(df):
-    # Initial assignment
-    df['Station'] = 'p1'
-    
-    # Vehicle section assignments
-    veh_mask = df['Description'].str.contains('prep|test', case=False)
-    df.loc[veh_mask, 'Station'] = df['Description']
+import plotly.graph_objects as go
 
-    assign_children_for_mask(veh_mask, df)
+def visualize_connections(df):
+    fig = go.Figure()
 
-    # Process final assemblies
-    final_assy_mask = df['Description'].str.contains('final')
-    df.loc[final_assy_mask, 'Station'] = df['Description'][final_assy_mask].apply(process_final_assembly)
+    assemblies = df['AssemblyID'].unique()
+    for assembly in assemblies:
+        subset_df = df[df['AssemblyID'] == assembly]
+        start_row = subset_df[subset_df['Interaction'] == 'Start']
+        complete_row = subset_df[subset_df['Interaction'] == 'Complete']
 
-    assign_children_for_mask(final_assy_mask, df)
+        if not start_row.empty and not complete_row.empty:
+            fig.add_trace(go.Scatter(
+                x=[start_row['Timestamp'].values[0], complete_row['Timestamp'].values[0]],
+                y=[start_row['AssemblyID'].values[0], complete_row['AssemblyID'].values[0]],
+                mode='lines+markers',
+                name=f"Assembly {assembly}"
+            ))
 
-    df.loc[final_assy_mask, 'Station'] = df['Description'][final_assy_mask].apply(process_final_assembly)
+    fig.update_layout(title="Connections between Start and Complete of Assemblies",
+                      xaxis_title="Timestamp",
+                      yaxis_title="AssemblyID")
 
-    # Process platforms
-    platform_mask = df['Description'].str.contains('platform', case=False)
-    df.loc[platform_mask, 'Station'] = 'Platform'
+    fig.show()
 
-    # Dictionary mappings
-    station_names = {
-        'Station 1': ['final assy 1', 'final assembly 1', 'final assembly station 1'],
-        'Station 2': ['final assy 2', 'final assembly 1'],
-        'Station 3': ['final assy 3', 'final assembly 3', 'final assembly station 3'],
-    }
+# Sample data
+df = pd.DataFrame({
+    'Timestamp': ['2023-01-01', '2023-01-02', '2023-01-03', '2023-01-04'],
+    'Interaction': ['Start', 'Complete', 'Start', 'Complete'],
+    'AssemblyID': [1, 1, 2, 2]
+})
 
-    for name, variations in station_names.items():
-        for variation in variations:
-            df['Station'] = df['Station'].str.replace(variation, name, regex=False)
-
-    # Turret and hull assignments
-    for item, station in [('t', 'pa'), ('l', 'pl')]:
-        ids = df[df['Description'].str.contains(item)]['mbomID'].tolist()
-        df.loc[df['mbomID'].isin(ids), 'Station'] = station
-        
-        assign_children_by_ids(ids, df, station)
-
-    return df
-
-def assign_children_for_mask(mask, df):
-    ids = df[mask]['mbomID'].tolist()
-    assign_children_by_ids(ids, df, df.loc[mask, 'Station'])
-
-def assign_children_by_ids(ids, df, station):
-    while ids:
-        children_ids = df[df['ParentID'].isin(ids)]['mbomID'].tolist()
-        df.loc[df['mbomID'].isin(children_ids), 'Station'] = station
-        ids = children_ids
-
-def process_final_assembly(desc):
-    station_mapping = {
-        '12': '1',
-        '23': '2',
-        '34': '3',
-        '45': '4',
-        '56': '5',
-        '67': '6',
-        '78': '7',
-        '910': '9',
-        '1112': '11'
-    }
-    
-    pattern = r'\b(' + '|'.join(sorted(map(str, station_mapping.keys()), key=len, reverse=True)) + r')\b'    
-    return re.sub(pattern, lambda m: station_mapping[m.group(1)], desc)
+visualize_connections(df)
