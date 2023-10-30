1f461e9e41186cd4487a083b19d411de7e183180
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-10-30 14:18:26 -0400

    Update s.py

--- s.py
+++ s.py
@@ -1,6 +1,8 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import plotly.graph_objs as go
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
 
 # Sample dataframe
 data = {'AssemblyID': [1, 2, 1, 2, 1, 2, 1, 2],
@@ -10,45 +12,47 @@ data = {'AssemblyID': [1, 2, 1, 2, 1, 2, 1, 2],
         'Vehicle': [1, 1, 1, 1, 2, 2, 2, 2]}
 
 df = pd.DataFrame(data)
-
-fig, ax = plt.subplots()
 stations = df['Station'].unique()
 num_stations = len(stations)
 
-# Create a dictionary to map stations to y-values
-station_dict = {station: i for i, station in enumerate(stations)}
+# Set up the Dash app
+app = dash.Dash(__name__)
 
-# Plot initialization function
-def init():
-    ax.clear()
-    ax.set_xlim(0, df['Timestamp'].max() + 2)
-    ax.set_ylim(0, num_stations)
-    ax.set_yticks(range(num_stations))
-    ax.set_yticklabels(stations)
-    ax.set_xlabel('Time')
-    ax.set_title('Vehicles Moving Through Stations')
+app.layout = html.Div([
+    dcc.Graph(id='live-graph'),
+    dcc.Interval(
+        id='interval-component',
+        interval=10*60*1000,  # 10 minutes in milliseconds
+        n_intervals=0
+    )
+])
 
-# Update function for the animation
-def update(frame):
-    ax.clear()
-    current_time = frame / 10  # Adjust this for faster/slower animation
-    ongoing_df = df[df['Timestamp'] <= current_time]
+@app.callback(Output('live-graph', 'figure'),
+              [Input('interval-component', 'n_intervals')])
+def update_graph(n):
+    time_passed = n * 10  # 10 minutes per interval
+    ongoing_df = df[df['Timestamp'] <= time_passed]
     
+    traces = []
     for _, row in ongoing_df.iterrows():
-        y = station_dict[row['Station']]
+        y = list(stations).index(row['Station'])
         if row['Interaction'] == 'start':
             color = 'green'
         else:
             color = 'red'
-        ax.scatter(row['Timestamp'], y, color=color, label=row['Vehicle'])
+        trace = go.Scatter(x=[row['Timestamp']], y=[y],
+                           mode='markers',
+                           marker=dict(color=color),
+                           name=f"Vehicle {row['Vehicle']}")
+        traces.append(trace)
     
-    ax.set_xlim(0, df['Timestamp'].max() + 2)
-    ax.set_ylim(0, num_stations)
-    ax.set_yticks(range(num_stations))
-    ax.set_yticklabels(stations)
-    ax.set_xlabel('Time')
-    ax.set_title('Vehicles Moving Through Stations at time {:.2f}'.format(current_time))
+    layout = go.Layout(title='Vehicles Moving Through Stations',
+                       xaxis=dict(title='Time (hours)'),
+                       yaxis=dict(tickvals=list(range(num_stations)),
+                                  ticktext=stations, title='Stations'),
+                       showlegend=False)
+    
+    return {'data': traces, 'layout': layout}
 
-ani = FuncAnimation(fig, update, frames=int(df['Timestamp'].max()*10 + 20), init_func=init, blit=False, repeat=True)
-plt.tight_layout()
-plt.show()
+if __name__ == '__main__':
+    app.run_server(debug=True)
