d14cc4dfb6b188fd5326a409ce8b69007aea589c
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-10-31 17:44:05 -0400

    Update s.py

--- s.py
+++ s.py
@@ -1,58 +1,33 @@
-import pandas as pd
-import plotly.graph_objs as go
-import dash
-from dash import dcc, html
-from dash.dependencies import Input, Output
+# Separate start and end interactions
+start_df = df[df['Interaction'] == 'start']
+end_df = df[df['Interaction'] == 'end']
 
-# Sample dataframe
-data = {'AssemblyID': [1, 2, 1, 2, 1, 2, 1, 2],
-        'Interaction': ['start', 'start', 'end', 'end', 'start', 'start', 'end', 'end'],
-        'Station': ['station 1', 'station 2', 'station 1', 'station 2', 'station 1', 'station 2', 'station 1', 'station 2'],
-        'Timestamp': [0.000, 0.000, 2.000, 3.000, 3.000, 5.000, 8.000, 9.000],
-        'Vehicle': [1, 1, 1, 1, 2, 2, 2, 2]}
+# Merge on AssemblyID, Station, and Vehicle
+merged_df = pd.merge(start_df, end_df, on=['AssemblyID', 'Station', 'Vehicle'], suffixes=('_start', '_end'))
 
-df = pd.DataFrame(data)
-stations = df['Station'].unique()
-num_stations = len(stations)
+# Extract necessary columns
+final_df = merged_df[['AssemblyID', 'Station', 'Timestamp_start', 'Timestamp_end', 'Vehicle']]
+import plotly.graph_objects as go
 
-# Set up the Dash app
-app = dash.Dash(__name__)
+fig = go.Figure()
 
-app.layout = html.Div([
-    dcc.Graph(id='live-graph'),
-    dcc.Interval(
-        id='interval-component',
-        interval=10*60*1000,  # 10 minutes in milliseconds
-        n_intervals=0
-    )
-])
+# Add a trace for each vehicle
+for vehicle in final_df['Vehicle'].unique():
+    vehicle_df = final_df[final_df['Vehicle'] == vehicle]
+    for _, row in vehicle_df.iterrows():
+        fig.add_trace(go.Scatter(
+            x=[row['Timestamp_start'], row['Timestamp_end']],
+            y=[row['Station'], row['Station']],
+            mode='lines',
+            name=f'Vehicle {vehicle}',
+            line=dict(width=10)
+        ))
 
-@app.callback(Output('live-graph', 'figure'),
-              [Input('interval-component', 'n_intervals')])
-def update_graph(n):
-    time_passed = n * 10  # 10 minutes per interval
-    ongoing_df = df[df['Timestamp'] <= time_passed]
-    
-    traces = []
-    for _, row in ongoing_df.iterrows():
-        y = list(stations).index(row['Station'])
-        if row['Interaction'] == 'start':
-            color = 'green'
-        else:
-            color = 'red'
-        trace = go.Scatter(x=[row['Timestamp']], y=[y],
-                           mode='markers',
-                           marker=dict(color=color),
-                           name=f"Vehicle {row['Vehicle']}")
-        traces.append(trace)
-    
-    layout = go.Layout(title='Vehicles Moving Through Stations',
-                       xaxis=dict(title='Time (hours)'),
-                       yaxis=dict(tickvals=list(range(num_stations)),
-                                  ticktext=stations, title='Stations'),
-                       showlegend=False)
-    
-    return {'data': traces, 'layout': layout}
+fig.update_layout(
+    title='Assembly Interactions by Vehicle and Station',
+    xaxis_title='Timestamp',
+    yaxis_title='Station',
+    yaxis_categoryorder='total descending'
+)
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+fig.show()
