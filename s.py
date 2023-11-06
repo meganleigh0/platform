2de8d9a91481c218c4928d73f56bf4339c4d2260
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-11-06 08:33:15 -0500

    Update s.py

--- s.py
+++ s.py
@@ -10,40 +10,29 @@ df = pd.DataFrame({
     'Vehicle': ['bus1', 'car1', 'bus1', 'bus1', 'bus1', 'car1']
 })
 
+# Group and aggregate data
+agg_df = df.groupby(['Vehicle', 'Station'])[['Timestamp_start', 'Timestamp_end']].agg({'Timestamp_start': 'min', 'Timestamp_end': 'max'}).reset_index()
+
 # Assign unique colors to each vehicle
 colors = {vehicle: f'rgb({hash(vehicle) % 256}, {hash(vehicle*2) % 256}, {hash(vehicle*3) % 256})' for vehicle in df['Vehicle'].unique()}
 
 # Initialize figure
 fig = go.Figure()
 
-# Track the vehicles added to the legend
-legend_added = []
-
-for vehicle, group in df.groupby('Vehicle'):
+for vehicle in agg_df['Vehicle'].unique():
     color = colors[vehicle]
-    # For each vehicle, add all its corresponding bars
-    for i, row in group.iterrows():
-        showlegend = False
-
-        # If the vehicle is not in the legend, add it
-        if vehicle not in legend_added:
-            showlegend = True
-            legend_added.append(vehicle)
-
-        # Create the bar for the Gantt chart
-        fig.add_trace(go.Bar(
-            x=[row['Timestamp_end'] - row['Timestamp_start']],  # Duration of the task
-            y=[row['Station']],
-            name=vehicle,  # This will put the vehicle name in the legend
-            orientation='h',
-            marker=dict(color=color),
-            showlegend=showlegend,
-            legendgroup=vehicle,  # Group by vehicle for the legend
-            width=0.4,  # You can adjust the width for better spacing
-            base=row['Timestamp_start'],  # Start time of the task
-            text=row['Assembly'],  # The assembly name written on the bar
-            hoverinfo='text+name',  # Show both vehicle and assembly as hover info
-        ))
+    vehicle_data = agg_df[agg_df['Vehicle'] == vehicle]
+    
+    # Create the Gantt chart bars
+    fig.add_trace(go.Bar(
+        x=vehicle_data['Timestamp_end'] - vehicle_data['Timestamp_start'],  # Duration of the task
+        y=vehicle_data['Station'],
+        name=vehicle,
+        orientation='h',
+        marker=dict(color=color),
+        text=vehicle_data['Vehicle'],  # Display the vehicle name
+        hoverinfo='text+name',
+    ))
 
 # Update layout
 fig.update_layout(
@@ -51,57 +40,7 @@ fig.update_layout(
     xaxis_title='Time',
     yaxis_title='Station',
     barmode='stack',
-    bargap=0.1,  # Gap between bars of adjacent location coordinates.
-    legend_title_text='Vehicle'
-)
-
-# Show the figure
-fig.show()
-
-
-
-# Filter DataFrame for Station 1 only
-df_station1 = df[df['Station'] == 'Station 1']
-
-# Assign unique colors to each vehicle
-colors = {vehicle: f'rgb({hash(vehicle) % 256}, {hash(vehicle*2) % 256}, {hash(vehicle*3) % 256})' for vehicle in df_station1['Vehicle'].unique()}
-
-# Initialize figure
-fig = go.Figure()
-
-# Track the vehicles added to the legend
-legend_added = []
-
-for index, row in df_station1.iterrows():
-    vehicle = row['Vehicle']
-    color = colors[vehicle]
-    showlegend = False
-
-    # If the vehicle is not in the legend, add it
-    if vehicle not in legend_added:
-        showlegend = True
-        legend_added.append(vehicle)
-
-    # Create the bar for the bar chart
-    fig.add_trace(go.Bar(
-        x=[row['Timestamp_end'] - row['Timestamp_start']],  # Duration of the task
-        y=[row['Assembly']],  # Use Assembly as the y-axis category
-        name=vehicle,  # This will put the vehicle name in the legend
-        orientation='h',
-        marker=dict(color=color),
-        showlegend=showlegend,
-        width=0.4,  # You can adjust the width for better spacing
-        base=row['Timestamp_start'],  # Start time of the task
-        hoverinfo='text+name',  # Show both vehicle and assembly as hover info
-    ))
-
-# Update layout
-fig.update_layout(
-    title='Station 1 Assembly Schedule',
-    xaxis_title='Time',
-    yaxis_title='Assembly',
-    barmode='stack',
-    bargap=0.1,  # Gap between bars of adjacent location coordinates.
+    bargap=0.1,
     legend_title_text='Vehicle'
 )
 
