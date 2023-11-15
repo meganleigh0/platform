f8e03662d259cb4e4af65ffa777e202a64cf89ee
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-11-15 09:02:47 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,38 +1,47 @@
 class Scheduler:
     SHIFT_DURATION = 8  # 8 hours shift
 
-    def __init__(self, env, schedule, day_rate):
-        # ... [other attributes]
-        self.shift_end = 0
+    def __init__(self, env, schedule, day_rate, plant_simulation):
+        self.env = env
+        self.schedule = schedule
+        self.day_rate = day_rate
+        self.plant_simulation = plant_simulation
+        self.current_month = 0
 
     def run_for_a_day(self):
-        self.shift_end = self.env.now + self.SHIFT_DURATION
-        while self.env.now < self.shift_end:
-            if not self.has_pending_processes():
-                break
+        shift_end = self.env.now + self.SHIFT_DURATION
+        while self.env.now < shift_end:
+            for _ in range(self.day_rate):
+                if not self.has_pending_processes():
+                    return  # Exit if no more processes are pending
 
-            program, mbom = self.get_next_program()
-            if program:
-                structure = plant_simulation.induce_structure()
-                product = load_product(self.env, mbom)
-                process_duration = self.get_process_duration(program)  # Define this method based on your logic
-                yield self.env.process(plant_simulation.process(structure, product, process_duration))
-            else:
-                yield self.env.timeout(1)  # Wait for an hour before checking again
+                program, mbom = self.get_next_program()
+                if program:
+                    structure = self.plant_simulation.induce_structure()
+                    product = load_product(self.env, mbom)
+                    self.env.process(self.plant_simulation.process(structure, product))
+
+            yield self.env.timeout(1)  # Wait for an hour before checking again
 
     def has_pending_processes(self):
-        # Check if there are any pending processes that need to be started in the current shift
         for (program, mbom), program_obj in self.schedule.programs.items():
             month = production_months[self.current_month]
             if program_obj.get_quantity_for_month(month) > 0:
                 return True
         return False
 
-    # ... [rest of the class]
+    def get_next_program(self):
+        for (program, mbom), program_obj in self.schedule.programs.items():
+            month = production_months[self.current_month]
+            qty = program_obj.get_quantity_for_month(month)
+            if qty > 0:
+                program_obj.production_plan[month] -= 1
+                return program, mbom
+        return None, None
 
-# Usage in your simulation
-env = simpy.Environment()
-# ... [setup your schedule and plant_simulation]
-scheduler = Scheduler(env, schedule, day_rate=1)
-scheduler.run_for_a_day()
-env.run()
+    def run_for_a_month(self):
+        for day in range(30):  # Assuming a 30-day month
+            self.env.run(until=self.env.now + self.SHIFT_DURATION)
+            self.current_month += 1 / 30  # Increment the month fractionally
+
+    # ... [rest of the class]
