2d0a70ed2136b76426a1dcd6ebf3d0021652065c
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-11-15 08:45:54 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,51 +1,38 @@
-import simpy
-
 class Scheduler:
+    SHIFT_DURATION = 8  # 8 hours shift
+
     def __init__(self, env, schedule, day_rate):
-        self.env = env
-        self.schedule = schedule
-        self.day_rate = day_rate
-        self.current_day = 1
-        self.current_month = 0
-
-    def start(self):
-        self.env.process(self.run_schedule())
-
-    def run_schedule(self):
-        while self.current_month < len(production_months):
-            yield self.env.process(self.run_for_a_month())
-            self.current_month += 1
-            self.current_day = 1
-
-    def run_for_a_month(self):
-        while self.current_day <= len(production_months[self.current_month]):
-            yield self.env.process(self.run_for_a_day())
-            self.current_day += 1
+        # ... [other attributes]
+        self.shift_end = 0
 
     def run_for_a_day(self):
-        for _ in range(self.day_rate):
+        self.shift_end = self.env.now + self.SHIFT_DURATION
+        while self.env.now < self.shift_end:
+            if not self.has_pending_processes():
+                break
+
             program, mbom = self.get_next_program()
             if program:
                 structure = plant_simulation.induce_structure()
                 product = load_product(self.env, mbom)
-                yield self.env.process(plant_simulation.process(structure, product))
+                process_duration = self.get_process_duration(program)  # Define this method based on your logic
+                yield self.env.process(plant_simulation.process(structure, product, process_duration))
+            else:
+                yield self.env.timeout(1)  # Wait for an hour before checking again
 
-    def get_next_program(self):
+    def has_pending_processes(self):
+        # Check if there are any pending processes that need to be started in the current shift
         for (program, mbom), program_obj in self.schedule.programs.items():
             month = production_months[self.current_month]
-            qty = program_obj.get_quantity_for_month(month)
-            if qty > 0:
-                program_obj.production_plan[month] -= 1
-                return program, mbom
-        return None, None
+            if program_obj.get_quantity_for_month(month) > 0:
+                return True
+        return False
+
+    # ... [rest of the class]
 
 # Usage in your simulation
 env = simpy.Environment()
-schedule = Schedule()
-schedule = schedule.load_schedule()
-plant_simulation = Plant(env)
-
+# ... [setup your schedule and plant_simulation]
 scheduler = Scheduler(env, schedule, day_rate=1)
-scheduler.start()
-
+scheduler.run_for_a_day()
 env.run()
