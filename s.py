3660710cdcfd01fa3af5dedba3819281f10774d4
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-11-14 19:59:33 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,45 +1,34 @@
+import simpy
+
 class Scheduler:
-    def __init__(self, schedule, day_rate):
+    def __init__(self, env, schedule, day_rate):
+        self.env = env
         self.schedule = schedule
         self.day_rate = day_rate
         self.current_day = 1
         self.current_month = 0
-        self.monthly_production_complete = False
 
-    def run_for_a_day(self, env, plant_simulation):
-        if not self.monthly_production_complete:
-            for _ in range(self.day_rate):
-                program, mbom = self.get_next_program()
-                if program:
-                    structure = plant_simulation.induce_structure()
-                    product = load_product(env, mbom)
-                    env.process(plant_simulation.process(structure, product))
-            self.current_day += 1
-            self.check_month_completion()
-        else:
-            print("Monthly production already complete.")
+    def start(self):
+        self.env.process(self.run_schedule())
 
-    def run_for_a_month(self, env, plant_simulation):
-        self.monthly_production_complete = False
-        while not self.monthly_production_complete:
-            self.run_for_a_day(env, plant_simulation)
-
-    def check_month_completion(self):
-        if self.current_day > len(production_months[self.current_month]):
-            self.current_day = 1
+    def run_schedule(self):
+        while self.current_month < len(production_months):
+            yield self.env.process(self.run_for_a_month())
             self.current_month += 1
-            if self.current_month >= len(production_months):
-                self.monthly_production_complete = True
-                print("Production for all months complete.")
-            else:
-                self.monthly_production_complete = self.is_monthly_production_complete()
+            self.current_day = 1
 
-    def is_monthly_production_complete(self):
-        for (program, mbom), program_obj in self.schedule.programs.items():
-            month = production_months[self.current_month]
-            if program_obj.get_quantity_for_month(month) > 0:
-                return False
-        return True
+    def run_for_a_month(self):
+        while self.current_day <= len(production_months[self.current_month]):
+            yield self.env.process(self.run_for_a_day())
+            self.current_day += 1
+
+    def run_for_a_day(self):
+        for _ in range(self.day_rate):
+            program, mbom = self.get_next_program()
+            if program:
+                structure = plant_simulation.induce_structure()
+                product = load_product(self.env, mbom)
+                yield self.env.process(plant_simulation.process(structure, product))
 
     def get_next_program(self):
         for (program, mbom), program_obj in self.schedule.programs.items():
@@ -49,3 +38,14 @@ class Scheduler:
                 program_obj.production_plan[month] -= 1
                 return program, mbom
         return None, None
+
+# Usage in your simulation
+env = simpy.Environment()
+schedule = Schedule()
+schedule = schedule.load_schedule()
+plant_simulation = Plant(env)
+
+scheduler = Scheduler(env, schedule, day_rate=1)
+scheduler.start()
+
+env.run()
