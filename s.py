75319bc2725cc6df310a416ae49a04ad260a6137
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-11-15 11:00:15 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,20 +1,40 @@
-class Scheduler:
-    SHIFT_DURATION = 8  # 8 hours shift
+import simpy
 
-    def __init__(self, env, schedule, day_rate, plant_simulation):
+class Scheduler:
+    SHIFT_DURATION = 8  # 8 hours
+
+    def __init__(self, env, schedule, day_rate):
         self.env = env
         self.schedule = schedule
         self.day_rate = day_rate
-        self.plant_simulation = plant_simulation
+        self.current_day = 1
         self.current_month = 0
 
+    def start(self):
+        self.env.process(self.run_schedule())
+
+    def run_schedule(self):
+        while self.current_month < len(production_months):
+            yield self.env.process(self.run_for_a_month())
+            self.current_month += 1
+            self.current_day = 1
+
+    def run_for_a_month(self):
+        while self.current_day <= len(production_months[self.current_month]):
+            yield self.env.process(self.run_for_a_day())
+            self.current_day += 1
+
     def run_for_a_day(self):
-        for _ in range(self.day_rate):
-            program, mbom = self.get_next_program()
-            if program:
-                structure = self.plant_simulation.induce_structure()
-                product = load_product(self.env, mbom)
-                self.env.process(self.plant_simulation.process(structure, product))
+        start_time = self.env.now
+        for shift in range(24 // self.SHIFT_DURATION):  # Number of shifts in a day
+            for _ in range(self.day_rate):
+                program, mbom = self.get_next_program()
+                if program:
+                    structure = plant_simulation.induce_structure()
+                    product = load_product(self.env, mbom)
+                    self.env.process(plant_simulation.process(structure, product))
+            # Wait for the next shift
+            yield self.env.timeout(start_time + self.SHIFT_DURATION * (shift + 1) - self.env.now)
 
     def get_next_program(self):
         for (program, mbom), program_obj in self.schedule.programs.items():
@@ -25,8 +45,10 @@ class Scheduler:
                 return program, mbom
         return None, None
 
-    def run_for_a_month(self):
-        for day in range(30):  # Assuming a 30-day month
-            self.run_for_a_day()
-            self.env.run(until=self.env.now + self.SHIFT_DURATION)
-            self.current_month += 1 / 30  # Increment the month fractionally
+# Usage in your simulation
+env = simpy.Environment()
+schedule = Schedule()
+schedule = schedule.load_schedule()
+plant_simulation = Plant(env)
+
+scheduler = Scheduler(env, schedule, day_rate=2)  #⬤
