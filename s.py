8e86d98023b126403fba7101a02acdc501c8783a
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-11-19 16:40:45 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,60 +1,15 @@
-import simpy
-
 class Scheduler:
-    SHIFT_DURATION = 8  # 8 hours
-
-    def __init__(self, env, schedule, day_rate):
+    def __init__(self, env, schedule, plant_simulation):
         self.env = env
         self.schedule = schedule
-        self.day_rate = day_rate
-        self.current_day = 1
-        self.current_month = 0
+        self.plant_simulation = plant_simulation
 
-    def start(self):
-        self.env.process(self.run_schedule())
-
-    def run_schedule(self):
-        while self.current_month < len(production_months):
-            yield self.env.process(self.run_for_a_month())
-            self.current_month += 1
-            self.current_day = 1
-
-    def run_for_a_month(self):
-        while self.current_day <= len(production_months[self.current_month]):
-            # Start the daily process and wait for it to complete
-            yield self.env.process(self.run_daily_process())
-            self.current_day += 1
-
-    def run_daily_process(self):
-        # Run the process for the day and wait until the next day
-        yield self.env.process(self.run_for_a_day())
-        yield self.env.timeout(24 - self.SHIFT_DURATION)  # Wait for the remainder of the 24-hour day
-
-    def run_for_a_day(self):
-        start_time = self.env.now
-        for shift in range(24 // self.SHIFT_DURATION):  # Number of shifts in a day
-            for _ in range(self.day_rate):
-                program, mbom = self.get_next_program()
-                if program:
-                    structure = plant_simulation.induce_structure()
+    def run_day(self, production_months):
+        for month in production_months:
+            for (program, mbom), program_instance in self.schedule.programs.items():
+                qty = program_instance.get_quantity_for_month(month)
+                for _ in range(qty):
+                    structure = self.plant_simulation.induce_structure()
                     product = load_product(self.env, mbom)
-                    self.env.process(plant_simulation.process(structure, product))
-            # Wait for the next shift
-            yield self.env.timeout(start_time + self.SHIFT_DURATION * (shift + 1) - self.env.now)
-
-    def get_next_program(self):
-        for (program, mbom), program_obj in self.schedule.programs.items():
-            month = production_months[self.current_month]
-            qty = program_obj.get_quantity_for_month(month)
-            if qty > 0:
-                program_obj.production_plan[month] -= 1
-                return program, mbom
-        return None, None
-
-# Usage in your simulation
-env = simpy.Environment()
-schedule = Schedule()
-schedule = schedule.load_schedule()
-plant_simulation = Plant(env)
-
-scheduler = Scheduler(env, schedule, day_rate=2⬤
+                    self.env.process(self.plant_simulation.process(structure, product))
+        self.env.run(8)
