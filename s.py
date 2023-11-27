d52ad192189ff5e719c57731abea884fe4c77b00
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-11-27 13:05:13 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,65 +1,37 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.express as px
-from typing import Any, Dict
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from analysis import visualization as viz
+from sim import main
 
-class Logger:
-    def __init__(self):
-        self.log = []
+# Initialize the Dash app
+app = dash.Dash(__name__)
 
-    def log_entry(self, timestamp: Any, interaction: str, **kwargs: Any) -> None:
-        """
-        Logs an interaction with additional key-value pairs.
+# Define the layout of the app
+app.layout = html.Div([
+    html.H1("Manufacturing Simulation Dashboard"),
+    html.Button("Run Simulation", id='run-simulation-button'),
+    dcc.Graph(id='gantt-chart'),
+    dcc.Graph(id='department-graph')
+])
 
-        :param timestamp: The timestamp of the interaction.
-        :param interaction: Description of the interaction.
-        :param kwargs: Additional key-value pairs relevant to the interaction.
-        """
-        entry = {'Timestamp': timestamp, 'Interaction': interaction, **kwargs}
-        self.log.append(entry)
+# Callback for running the simulation
+@app.callback(
+    [Output('gantt-chart', 'figure'),
+     Output('department-graph', 'figure')],
+    [Input('run-simulation-button', 'n_clicks')]
+)
+def update_output(n_clicks):
+    if n_clicks is not None:
+        # Run the simulation
+        main.run_simulation()
+        # Generate visualizations
+        gantt_chart = viz.gantt()
+        department_graph = viz.department_graph()
+        return gantt_chart, department_graph
+    return dash.no_update
 
-    def to_dataframe(self) -> pd.DataFrame:
-        """
-        Converts the log entries to a pandas DataFrame.
-
-        :return: DataFrame of log entries.
-        """
-        return pd.DataFrame(self.log)
-
-    def print_table(self) -> None:
-        """
-        Prints the log entries in a table format.
-        """
-        df = self.to_dataframe()
-        print(df)
-
-    def visualize(self, x_col: str, y_col: str, title: str, kind: str = 'line') -> None:
-        """
-        Visualizes the log data.
-
-        :param x_col: The column to use for the x-axis.
-        :param y_col: The column to use for the y-axis.
-        :param title: The title of the visualization.
-        :param kind: The kind of plot (e.g., 'line', 'scatter').
-        """
-        df = self.to_dataframe()
-
-        if kind == 'line':
-            plt.plot(df[x_col], df[y_col], '-o')
-            plt.xlabel(x_col)
-            plt.ylabel(y_col)
-            plt.title(title)
-            plt.grid(True)
-            plt.show()
-        elif kind == 'scatter':
-            fig = px.scatter(df, x=x_col, y=y_col, color=y_col)
-            fig.show()
-        else:
-            print(f"Visualization kind '{kind}' not supported.")
-
-# Example usage
-logger = Logger()
-logger.log_entry("2023-01-01 10:00", "Started Process", Process="Cutting", Department="Manufacturing")
-logger.log_entry("2023-01-01 10:05", "Completed Process", Process="Cutting", Department="Manufacturing")
-logger.print_table()
-logger.visualize(x_col='Timestamp', y_col='Process', title='Process Timeline', kind='line')
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
