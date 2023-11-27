50ceb2ca42fcb485753dc9d81c47440d661372d7
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-11-27 13:40:55 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,17 +1,30 @@
+import dash
+from dash import html, dcc, dash_table
+from dash.dependencies import Input, Output, State
+
 # Initialize the Dash app
 app = dash.Dash(__name__)
 
 # Initialize the Schedule object and load data
 schedule = Schedule()
-df = schedule.load_schedule()
+schedule.load_schedule()  # Load initial data into the schedule
+
+# Function to convert schedule data to a format suitable for Dash DataTable
+def generate_table_data():
+    data = []
+    for (program, mbom), prog_obj in schedule.programs.items():
+        row = {'Program': program, 'MBOM': mbom}
+        for month in schedule.months:
+            row[month] = prog_obj.get_quantity_for_month(month)
+        data.append(row)
+    return data
 
 # App layout
 app.layout = html.Div([
     html.H1("Program Schedule"),
-    dash_table.DataTable(id='schedule-table', columns=[{"name": i, "id": i} for i in df.columns], data=df.to_dict('records')),
+    dash_table.DataTable(id='schedule-table', columns=[{"name": "Program", "id": "Program"}, {"name": "MBOM", "id": "MBOM"}] + [{"name": month, "id": month} for month in schedule.months]),
     html.H3("Update Schedule"),
     dcc.Input(id='program-input', type='text', placeholder='Program Name'),
-    dcc.Input(id='mbom-input', type='text', placeholder='MBOM'),
     dcc.Input(id='month-input', type='text', placeholder='Month'),
     dcc.Input(id='quantity-input', type='number', placeholder='Quantity'),
     html.Button('Update', id='update-button'),
@@ -23,14 +36,12 @@ app.layout = html.Div([
     Output('schedule-table', 'data'),
     [Input('update-button', 'n_clicks')],
     [State('program-input', 'value'),
-     State('mbom-input', 'value'),
      State('month-input', 'value'),
      State('quantity-input', 'value')])
-def update_schedule_display(n_clicks, program, mbom, month, quantity):
+def update_schedule_display(n_clicks, program, month, quantity):
     if n_clicks:
         schedule.update_schedule(program, month, quantity)
-    df = schedule.load_schedule()  # Reload the updated schedule
-    return df.to_dict('records')
+    return generate_table_data()
 
 # Run the app
 if __name__ == '__main__':
