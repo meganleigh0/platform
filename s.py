2bb180c232bbd85f5cc3f82d0fcf0717884e02db
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-11-28 11:23:34 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,11 +1,30 @@
-@app.callback(
-    Output('schedule-table', 'data'),
-    [Input('update-button', 'n_clicks')],
-    [State('program-dropdown', 'value'),
-     State('mbom-dropdown', 'value'),  # Assuming you have a dropdown to select MBOM
-     State('month-dropdown', 'value'),
-     State('quantity-input', 'value')])
-def update_schedule_display(n_clicks, program, mbom, month, quantity):
-    if n_clicks:
-        schedule.update_schedule(program, mbom, month, quantity)
-    return generate_table_data()
+from dash import html, dcc
+from dash.dependencies import Input, Output
+from app import app
+import pages.page1, pages.page2  # Import other pages as needed
+
+# Define the navigation bar
+navbar = html.Div([
+    dcc.Link('Home', href='/'),
+    dcc.Link('Page 1', href='/page1'),
+    dcc.Link('Page 2', href='/page2'),
+    # Add more links as needed
+], className='navbar')
+
+# Main layout of the app
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    navbar,
+    html.Div(id='page-content')
+])
+
+# Callback to update the page content
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/page1':
+        return pages.page1.layout
+    elif pathname == '/page2':
+        return pages.page2.layout
+    else:
+        return "Welcome to the Home Page"
