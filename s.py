6a5a646a6164b6c070d5d5b863dce09e9af2b93c
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-11-28 11:41:37 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,13 +1,30 @@
-from dash import Dash
-from flask import Flask
+from dash import html, dcc
+from dash.dependencies import Input, Output
+from app import app
+import pages.page1, pages.page2  # Import other pages as needed
 
-server = Flask(__name__)  # Create a Flask instance
+# Define the navigation bar
+navbar = html.Div([
+    dcc.Link('Home', href='/'),
+    dcc.Link('Page 1', href='/page1'),
+    dcc.Link('Page 2', href='/page2'),
+    # Add more links as needed
+], className='navbar')
 
-app = Dash(__name__, server=server, suppress_callback_exceptions=True)  # Initialize Dash app
+# Main layout of the app
+layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    navbar,
+    html.Div(id='page-content')
+])
 
-# Import layout and callbacks
-from index import layout
-app.layout = layout
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+# Callback to update the page content
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/page1':
+        return pages.page1.layout
+    elif pathname == '/page2':
+        return pages.page2.layout
+    else:
+        return "Welcome to the Home Page"
