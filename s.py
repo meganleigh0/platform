03684e3d038b99aa8dcd3650b76013c06e0628d4
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-11-28 12:10:22 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,4 +1,5 @@
-from dash import Dash
+from dash import Dash, html, dcc
+from dash.dependencies import Input, Output
 from flask import Flask
 
 # Initialize Flask server
@@ -7,42 +8,47 @@ server = Flask(__name__)
 # Initialize Dash app
 app = Dash(__name__, server=server, suppress_callback_exceptions=True)
 
-# Import the layout and register it with the app
-from layouts import layout
-app.layout = layout
-
-# Run the app
-if __name__ == '__main__':
-    app.run_server(debug=True)
-
-from dash import html, dcc
-
 # Define the navigation bar
 navbar = html.Div([
     dcc.Link('Home', href='/'),
     dcc.Link('Page 1', href='/page1'),
     dcc.Link('Page 2', href='/page2'),
-    # Add more links as needed
 ], className='navbar')
 
 # Define the layout
-layout = html.Div([
+app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     navbar,
     html.Div(id='page-content')
 ])
 
-from dash.dependencies import Input, Output
-from app import app
-import pages.page1, pages.page2  # Import other pages as needed
+# Define layouts for each page
+def get_layout_home():
+    return html.Div([
+        html.H1("Welcome to the Home Page")
+    ])
+
+def get_layout_page1():
+    return html.Div([
+        html.H1("Dash App - Page 1")
+    ])
+
+def get_layout_page2():
+    return html.Div([
+        html.H1("Dash App - Page 2")
+    ])
 
 # Callback to update the page content
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
 def display_page(pathname):
     if pathname == '/page1':
-        return pages.page1.layout
+        return get_layout_page1()
     elif pathname == '/page2':
-        return pages.page2.layout
-    else:
-        return "Welcome to the Home Page"
+        return get_layout_page2()
+    elif pathname in ['/', None]:
+        return get_layout_home()
+    return "404 Page Not Found"
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
