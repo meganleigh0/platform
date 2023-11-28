6ad651aca2d500fce10d26fad3d941df3cd919f1
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-11-28 14:37:33 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,39 +1,21 @@
-.main-container {
+.button {
+    background-color: #007bff; /* Primary color */
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
     font-family: 'Roboto', sans-serif;
-    color: #333;
-}
-
-.nav-bar {
-    background-color: #f8f9fa;
-    padding: 10px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
-
-.nav-link {
-    margin-right: 15px;
-    color: #007bff;
-    text-decoration: none;
+    font-size: 16px;
     font-weight: 500;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    margin: 4px 2px;
+    transition-duration: 0.4s;
+    cursor: pointer;
 }
 
-.nav-link:hover {
-    color: #0056b3;
-    text-decoration: underline;
+.button:hover {
+    background-color: #0056b3; /* Darker shade for hover */
+    color: white;
 }
-
-@media (max-width: 768px) {
-    .nav-link {
-        display: block;
-        margin-bottom: 10px;
-    }
-}
-
-app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div([
-        dcc.Link('Home', href='/', className='nav-link'),
-        dcc.Link('Page 1', href='/page1', className='nav-link'),
-        dcc.Link('Page 2', href='/page2', className='nav-link'),
-    ], className='nav-bar'),
-    html.Div(id='page-content')
-], className='main-container')
