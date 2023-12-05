8ac995d6a13b160ea8486b17ff6f8b218a9e1d1d
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-05 10:53:47 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,26 +1,17 @@
-import pandas as pd
-import plotly.express as px
 
-# Simulated data based on the user's description
-data = {
-    "Interaction": ["Plant 1 Status"] * 6,
-    "Timestamp": [0.00, 0.00, 0.00, 2.00, 3.00, 4.00],
-    "Turret_Completed": [0, 0, 0, 1, 1, 3],
-    "Turret_Processing": [1, 2, 3, 2, 3, 1],
-    "Hull_Completed": [0, 0, 0, 0, 1, 2],
-    "Hull_Processing": [1, 2, 3, 3, 2, 3]
-}
-
-df = pd.DataFrame(data)
-
-# Creating an animated bar chart using Plotly
+# Creating an animated bar plot
 fig = px.bar(df, 
              x="Timestamp", 
              y=["Turret_Completed", "Turret_Processing", "Hull_Completed", "Hull_Processing"],
              labels={"value": "Count", "variable": "Status"},
              animation_frame="Timestamp",
-             range_y=[0, df[["Turret_Completed", "Turret_Processing", "Hull_Completed", "Hull_Processing"]].values.max() + 1],
-             title="Simulation Data Over Time")
+             range_y=[0, df[["Turret_Completed", "Turret_Processing", "Hull_Completed", "Hull_Processing"]].values.max() + 1]
+)
 
-fig.update_layout(barmode='group')
+fig.update_layout(title="Simulation Data: Turrets and Hulls Status Over Time",
+                  xaxis_title="Timestamp",
+                  yaxis_title="Count",
+                  barmode='group')
+
+# Running the figure in your local Python environment
 fig.show()
