cd2db066a0d7f97b29f5b62536d27cbf04680a29
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-05 12:00:49 -0500

    Update s.py

--- s.py
+++ s.py
@@ -9,23 +9,25 @@ class Scheduler:
     def is_weekend(self, date):
         return date.weekday() > 4  # 5 and 6 correspond to Saturday and Sunday
 
+    def process_product(self, program_obj):
+        product = load_product(self.env, program_obj.mbom)
+        self.env.process(product.process())  # Start the product process
+
     def run_for_a_day(self, day_rate):
         for month in self.schedule.months:
             for program, program_obj in self.schedule.programs.items():
                 quantity_needed = program_obj.get_quantity_for_month(month)
                 if quantity_needed > 0:
-                    product = load_product(self.env, program_obj.mbom)
-                    self.env.process(product.process())
-                    program_obj.update_production_plan(month, quantity_needed - 1)
-                    if day_rate == 2 and quantity_needed > 1:
-                        product = load_product(self.env, program_obj.mbom)
-                        self.env.process(product.process())
-                        program_obj.update_production_plan(month, quantity_needed - 2)
-                    break  # Move to next day after processing day_rate products
+                    for _ in range(min(day_rate, quantity_needed)):
+                        self.process_product(program_obj)
+                        program_obj.update_production_plan(month, quantity_needed - 1)
+                    break  # Move to next day after processing up to day_rate products
 
     def run(self, day_rate, days_to_run):
         for _ in range(days_to_run):
             if not self.is_weekend(self.current_date):
                 self.run_for_a_day(day_rate)
+                self.env.run(until=self.env.now + 8)  # Simulate 8 hours of work
             self.current_date += datetime.timedelta(days=1)
-            self.env.run()  # Run the simulation for the current day
+
+# Usage remains the same
