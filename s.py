3f8cd5754c52c137032026b478d55e01aca144e6
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-05 11:01:56 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,17 +1,23 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
 
-# Creating an animated bar plot
-fig = px.bar(df, 
-             x="Timestamp", 
-             y=["Turret_Completed", "Turret_Processing", "Hull_Completed", "Hull_Processing"],
-             labels={"value": "Count", "variable": "Status"},
-             animation_frame="Timestamp",
-             range_y=[0, df[["Turret_Completed", "Turret_Processing", "Hull_Completed", "Hull_Processing"]].values.max() + 1]
-)
+# Set up the figure and axes for the animation
+fig, ax = plt.subplots()
+bar_labels = ['Hulls in Progress', 'Hulls Completed', 'Turrets in Progress', 'Turrets Completed']
+bars = ax.bar(bar_labels, [0, 0, 0, 0])
 
-fig.update_layout(title="Simulation Data: Turrets and Hulls Status Over Time",
-                  xaxis_title="Timestamp",
-                  yaxis_title="Count",
-                  barmode='group')
+# Set some properties for the plot
+ax.set_ylim(0, max([max([d[key] for key in d]) for d in parsed_data]) + 1)
+ax.set_ylabel('Count')
+ax.set_title('Simulation Progress of Hulls and Turrets')
 
-# Running the figure in your local Python environment
-fig.show()
+# Define the update function for the animation
+def update(frame):
+    data = parsed_data[frame]
+    for bar, label in zip(bars, bar_labels):
+        bar.set_height(data[label.lower().replace(' ', '_')])
+    return bars
+
+# Create the animation
+ani = animation.FuncAnimation(fig, update, frames=len(parsed_data), blit=True, repeat=False)
