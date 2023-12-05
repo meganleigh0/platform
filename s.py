6d386840b95919e9757e292fb444b8fa1b33345e
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-05 18:12:44 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,33 +1,55 @@
-import datetime
+Maintain a separate log or storage system for all parts by their part number, in addition to the individual part instances within each product.
 
-class Scheduler:
-    def __init__(self, schedule, env, start_date=datetime.date(2025, 1, 1)):
-        self.schedule = schedule
-        self.env = env
-        self.current_date = start_date
+Parts Database: Create a database or a structured data storage (like a DataFrame in pandas) that lists all parts with their part numbers, descriptions, current inventory levels, lead times, and supplier information.
 
-    def is_weekend(self, date):
-        return date.weekday() > 4  # 5 and 6 correspond to Saturday and Sunday
+Integration with Simulation: During the simulation, as each assembly process starts, update the need-by dates for each required part in the database based on the assembly start time and lead time of the parts.
 
-    def process_product(self, program_obj):
-        product = load_product(self.env, program_obj.mbom)
-        self.env.process(product.process())  # Start the product process
+Inventory Management Logic: Implement logic for inventory management, including rules for when to reorder parts based on need-by dates and current inventory levels.
 
-    def run_for_a_day(self, day_rate):
-        for month in self.schedule.months:
-            for program, program_obj in self.schedule.programs.items():
-                quantity_needed = program_obj.get_quantity_for_month(month)
-                if quantity_needed > 0:
-                    for _ in range(min(day_rate, quantity_needed)):
-                        self.process_product(program_obj)
-                        program_obj.update_production_plan(month, quantity_needed - 1)
-                    break  # Move to next day after processing up to day_rate products
+Post-Simulation Analysis: After the simulation, analyze the parts database to assess the performance of the supply chain. Look at metrics like on-time availability of parts, frequency of shortages, and efficiency of inventory turnover.
 
-    def run(self, day_rate, days_to_run):
-        for _ in range(days_to_run):
-            if not self.is_weekend(self.current_date):
-                self.run_for_a_day(day_rate)
-                self.env.run(until=self.env.now + 8)  # Simulate 8 hours of work
-            self.current_date += datetime.timedelta(days=1)
+Visualization Tools: Develop visualization tools to represent the status of the supply chain dynamically, showing real-time data on inventory levels, upcoming need-by dates, and order statuses.
 
-# Usage remains the same
+1. Design a Central Parts Database
+Structure: Create a centralized database or data structure (e.g., a pandas DataFrame) to store information about each part. Include columns for part number, current inventory level, and a list or array to store multiple need-by dates.
+
+Initialization: Initially populate this database with all parts, setting initial inventory levels and empty lists for need-by dates.
+
+2. Update Part Class
+Enhanced Tracking: Modify the Part class to include a method for updating its need-by date in the central database. This method should append the new need-by date to the existing list of dates for that part.
+
+3. Modify Assembly Processing
+Calculating Need-by Dates: When an assembly process starts, calculate the need-by date for each required part. This date is typically the assembly start time minus the lead time of the part.
+
+Updating Database: For each part required by the assembly, call the method in the Part class to update its need-by date in the central database.
+
+4. Inventory Management Logic
+Reorder Rules: Implement logic to determine when to reorder parts. This might be based on comparing the earliest need-by date for each part with its lead time and current inventory level.
+
+Dynamic Updates: As the simulation runs, continuously update inventory levels and reorder parts as necessary based on the need-by dates and reorder rules.
+
+5. Handling Multiple Need-by Dates
+Prioritization: In cases where a part has multiple need-by dates, prioritize based on the earliest date. Ensure that inventory levels meet the earliest demand first.
+
+Consolidation: Optionally, implement logic to consolidate orders for the same part with close need-by dates to optimize ordering and reduce costs.
+
+
+For your Supply Chain Management (SCM) module within the manufacturing simulation project, structuring the folder with appropriate classes and data management files is crucial for organization and efficiency. Here's a suggested structure for the SCM folder:
+
+SCM Folder Structure
+orders.py: A class or set of classes to handle orders.
+
+Order Class: Represents an individual order, including attributes like order ID, part number, quantity, order date, expected delivery date, and status (e.g., ordered, in transit, received).
+Order Management: Functions to place new orders, update order statuses, and manage the lifecycle of each order.
+inventory.py: For inventory management.
+
+Inventory Class: Manages the inventory levels of each part, including methods for updating inventory, checking stock levels, and determining when to reorder.
+Inventory Tracking: Functions to log changes in inventory, such as receiving new stock or deducting stock as parts are used in production.
+supplier.py: Represents suppliers.
+
+Supplier Class: Contains information about suppliers, such as name, reliability rating, lead times, and contact details.
+Supplier Management: Functions to manage supplier relationships, including performance tracking and supplier selection logic.
+parts_database.py: A module to manage the parts database.
+
+Parts Database: Ideally implemented as a DataFrame (or a similar structure), holding details like part number, description, current inventory level, lead times, associated suppliers, and a list of need-by dates.
+Database Management: Functions to update and query the parts database, including adding new need-by dates and updating inventory levels.
