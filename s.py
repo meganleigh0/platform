9e08d953ac104bc8174e2c5c69d0e28f67b352c2
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-05 11:57:47 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,54 +1,31 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+import datetime
 
+class Scheduler:
+    def __init__(self, schedule, env, start_date=datetime.date(2025, 1, 1)):
+        self.schedule = schedule
+        self.env = env
+        self.current_date = start_date
 
-# Function to parse the log data
-def parse_log_data(log_df):
-    counts = {
-        "hulls_in_progress": 0,
-        "hulls_completed": 0,
-        "turrets_in_progress": 0,
-        "turrets_completed": 0,
-    }
-    history = []
-    for _, row in log_df.iterrows():
-        interaction = row["Interaction"]
-        if "Start Plant 1 Hull" in interaction:
-            counts["hulls_in_progress"] += 1
-        elif "Start Plant 1 Turret" in interaction:
-            counts["turrets_in_progress"] += 1
-        elif "Complete Plant 1 Hull" in interaction:
-            counts["hulls_in_progress"] -= 1
-            counts["hulls_completed"] += 1
-        elif interaction == "Hull Status":
-            counts["hulls_in_progress"] = row.get("InProcessing", 0)
-            counts["hulls_completed"] = row.get("Completed", 0)
-        history.append(counts.copy())
-    return history
+    def is_weekend(self, date):
+        return date.weekday() > 4  # 5 and 6 correspond to Saturday and Sunday
 
-parsed_data = parse_log_data(log_df)
+    def run_for_a_day(self, day_rate):
+        for month in self.schedule.months:
+            for program, program_obj in self.schedule.programs.items():
+                quantity_needed = program_obj.get_quantity_for_month(month)
+                if quantity_needed > 0:
+                    product = load_product(self.env, program_obj.mbom)
+                    self.env.process(product.process())
+                    program_obj.update_production_plan(month, quantity_needed - 1)
+                    if day_rate == 2 and quantity_needed > 1:
+                        product = load_product(self.env, program_obj.mbom)
+                        self.env.process(product.process())
+                        program_obj.update_production_plan(month, quantity_needed - 2)
+                    break  # Move to next day after processing day_rate products
 
-# Set up the figure and axes for the animation
-fig, ax = plt.subplots()
-bar_labels = ['Hulls in Progress', 'Hulls Completed', 'Turrets in Progress', 'Turrets Completed']
-bars = ax.bar(bar_labels, [0, 0, 0, 0])
-ax.set_ylim(0, max([max([d[key] for key in d]) for d in parsed_data]) + 1)
-ax.set_ylabel('Count')
-ax.set_title('Simulation Progress of Hulls and Turrets')
-
-# Update function for the animation
-def update(frame):
-    data = parsed_data[frame]
-    for bar, label in zip(bars, bar_labels):
-        bar.set_height(data[label.lower().replace(' ', '_')])
-    return bars
-
-# Create the animation
-ani = animation.FuncAnimation(fig, update, frames=len(parsed_data), blit=True, repeat=False)
-
-# Save the animation as a video file
-ani.save('simulation_progress.mp4', writer='ffmpeg', fps=1)
-
-# Display the plot (for interactive environments)
-plt.show()
+    def run(self, day_rate, days_to_run):
+        for _ in range(days_to_run):
+            if not self.is_weekend(self.current_date):
+                self.run_for_a_day(day_rate)
+            self.current_date += datetime.timedelta(days=1)
+            self.env.run()  # Run the simulation for the current day
