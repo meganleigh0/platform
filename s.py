930fc1fbc459914a427e97aea597484da94154e1
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-13 14:41:52 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,55 +1,21 @@
-Maintain a separate log or storage system for all parts by their part number, in addition to the individual part instances within each product.
+def calculate_total_hours(operations_list):
+    # Initialize a dictionary to store total hours per department
+    department_hours = {}
 
-Parts Database: Create a database or a structured data storage (like a DataFrame in pandas) that lists all parts with their part numbers, descriptions, current inventory levels, lead times, and supplier information.
+    # Process each assembly
+    for assembly in operations_list:
+        # Flatten the assembly's operations
+        flattened_operations = [op for op in assembly]
 
-Integration with Simulation: During the simulation, as each assembly process starts, update the need-by dates for each required part in the database based on the assembly start time and lead time of the parts.
+        # Process each operation in the assembly
+        for operation in flattened_operations:
+            hours, name, department = operation
+            hours = float(hours)  # Convert hours to a float
 
-Inventory Management Logic: Implement logic for inventory management, including rules for when to reorder parts based on need-by dates and current inventory levels.
+            # Add hours to the respective department
+            if department in department_hours:
+                department_hours[department] += hours
+            else:
+                department_hours[department] = hours
 
-Post-Simulation Analysis: After the simulation, analyze the parts database to assess the performance of the supply chain. Look at metrics like on-time availability of parts, frequency of shortages, and efficiency of inventory turnover.
-
-Visualization Tools: Develop visualization tools to represent the status of the supply chain dynamically, showing real-time data on inventory levels, upcoming need-by dates, and order statuses.
-
-1. Design a Central Parts Database
-Structure: Create a centralized database or data structure (e.g., a pandas DataFrame) to store information about each part. Include columns for part number, current inventory level, and a list or array to store multiple need-by dates.
-
-Initialization: Initially populate this database with all parts, setting initial inventory levels and empty lists for need-by dates.
-
-2. Update Part Class
-Enhanced Tracking: Modify the Part class to include a method for updating its need-by date in the central database. This method should append the new need-by date to the existing list of dates for that part.
-
-3. Modify Assembly Processing
-Calculating Need-by Dates: When an assembly process starts, calculate the need-by date for each required part. This date is typically the assembly start time minus the lead time of the part.
-
-Updating Database: For each part required by the assembly, call the method in the Part class to update its need-by date in the central database.
-
-4. Inventory Management Logic
-Reorder Rules: Implement logic to determine when to reorder parts. This might be based on comparing the earliest need-by date for each part with its lead time and current inventory level.
-
-Dynamic Updates: As the simulation runs, continuously update inventory levels and reorder parts as necessary based on the need-by dates and reorder rules.
-
-5. Handling Multiple Need-by Dates
-Prioritization: In cases where a part has multiple need-by dates, prioritize based on the earliest date. Ensure that inventory levels meet the earliest demand first.
-
-Consolidation: Optionally, implement logic to consolidate orders for the same part with close need-by dates to optimize ordering and reduce costs.
-
-
-For your Supply Chain Management (SCM) module within the manufacturing simulation project, structuring the folder with appropriate classes and data management files is crucial for organization and efficiency. Here's a suggested structure for the SCM folder:
-
-SCM Folder Structure
-orders.py: A class or set of classes to handle orders.
-
-Order Class: Represents an individual order, including attributes like order ID, part number, quantity, order date, expected delivery date, and status (e.g., ordered, in transit, received).
-Order Management: Functions to place new orders, update order statuses, and manage the lifecycle of each order.
-inventory.py: For inventory management.
-
-Inventory Class: Manages the inventory levels of each part, including methods for updating inventory, checking stock levels, and determining when to reorder.
-Inventory Tracking: Functions to log changes in inventory, such as receiving new stock or deducting stock as parts are used in production.
-supplier.py: Represents suppliers.
-
-Supplier Class: Contains information about suppliers, such as name, reliability rating, lead times, and contact details.
-Supplier Management: Functions to manage supplier relationships, including performance tracking and supplier selection logic.
-parts_database.py: A module to manage the parts database.
-
-Parts Database: Ideally implemented as a DataFrame (or a similar structure), holding details like part number, description, current inventory level, lead times, associated suppliers, and a list of need-by dates.
-Database Management: Functions to update and query the parts database, including adding new need-by dates and updating inventory levels.
+    return department_hours
