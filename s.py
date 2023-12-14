71e7ba4c62025fb9dbd42989f111cc092f77bc74
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-14 17:56:07 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,44 +1,55 @@
-import dash
-from dash import dcc, html
-from dash.dependencies import Input, Output, State
-import plotly.graph_objs as go
-
-# Assuming the provided code and necessary libraries are imported
-simulation = Simulation()
-
-app = dash.Dash(__name__)
-
+def generate_schedule_table(schedule):
+    # Placeholder function - replace with actual logic to generate a table from the schedule
+    table_header = [html.Thead(html.Tr([html.Th("Program"), html.Th("Quantity per Month")]))]
+    table_body = [html.Tr([html.Td(program), html.Td(quantity)]) for program, quantity in schedule.items()]
+    return html.Table(table_header + table_body)
+Step 2: Dash App Layout
+python
+Copy code
 app.layout = html.Div([
     html.H1("Production Schedule Simulator"),
+    html.Div(id='schedule-table', children=generate_schedule_table(simulation.schedule.programs)),
     dcc.Dropdown(
         id='month-selector',
         options=[{'label': month, 'value': month} for month in simulation.schedule.months],
         value='January 2024'
     ),
-    dcc.Input(id='rate-input', type='number', value=5, placeholder='Enter Rate'),
     dcc.RadioItems(
         id='duration-selector',
         options=[{'label': 'Day', 'value': 1}, {'label': 'Week', 'value': 7}, {'label': 'Month', 'value': 30}],
         value=1
     ),
     html.Button('Run Simulation', id='run-simulation-btn'),
-    html.Div(id='schedule-table'),
+    html.Button('Clear Simulation', id='clear-simulation-btn'),
     dcc.Graph(id='gantt-chart', figure=go.Figure())
 ])
-
+Step 3: Callbacks for Simulation and Clearing
+python
+Copy code
 @app.callback(
-    Output('schedule-table', 'children'),
-    [Input('run-simulation-btn', 'n_clicks')],
+    [Output('gantt-chart', 'figure'),
+     Output('schedule-table', 'children')],
+    [Input('run-simulation-btn', 'n_clicks'),
+     Input('clear-simulation-btn', 'n_clicks')],
     [State('month-selector', 'value'),
-     State('rate-input', 'value'),
      State('duration-selector', 'value')]
 )
-def update_schedule_table(n_clicks, selected_month, rate, duration):
-    if n_clicks:
-        print("Simulation started")  # Logging to console
-        # Dummy data for testing
-        return html.Table([html.Tr([html.Td("Simulation Data Here")])])
-    return "Click 'Run Simulation' to display the schedule."
+def update_output(run_clicks, clear_clicks, selected_month, duration):
+    ctx = dash.callback_context
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+    if not ctx.triggered:
+        button_id = 'No clicks yet'
+    else:
+        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+
+    if button_id == 'run-simulation-btn' and run_clicks:
+        simulation.run_simulation(rate, duration, selected_month)
+        gantt_chart = viz.gantt()  # Assuming this returns a figure object
+        return gantt_chart, generate_schedule_table(simulation.schedule.programs)
+
+    elif button_id == 'clear-simulation-btn' and clear_clicks:
+        # Clearing logic here
+        return go.Figure(), generate_schedule_table(simulation.schedule.programs)
+
+    return go.Figure(), generate_schedule_table(simulation.schedule.programs)
+This setup:
