2253e0e075191f864eab3a67c2e9722632876c4e
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-14 17:31:27 -0500

    Update s.py

--- s.py
+++ s.py
@@ -2,41 +2,64 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import pandas as pd
 
-# Assuming the provided code and required libraries are imported
+# Assuming the provided code and necessary libraries are imported
+from datetime import datetime
+
+# Initialize the simulation
+simulation = Simulation()
 
 # Initialize the Dash app
 app = dash.Dash(__name__)
 
 # Define the layout of the app
 app.layout = html.Div([
-    html.H1("Production Schedule"),
+    html.H1("Production Schedule Calendar"),
     dcc.Dropdown(
         id='month-selector',
         options=[
-            {'label': month, 'value': month} for month in scheduler.schedule.months
+            {'label': month, 'value': month} for month in simulation.schedule.months
         ],
         value='January 2024'
     ),
     html.Button('Start Simulation', id='start-btn'),
-    html.Div(id='schedule-output')
+    html.Button('Reset Simulation', id='reset-btn'),
+    html.Div(id='calendar-output')
 ])
 
-# Callback for updating the schedule display
+# Function to create a calendar-like display
+def create_calendar(month, schedule_data):
+    # Convert schedule_data into a calendar format
+    # This is a placeholder and should be replaced with actual logic
+    calendar = []
+    for week in range(4):  # Assuming 4 weeks in a month for simplicity
+        week_row = html.Tr([html.Td(f"Week {week+1}")])
+        calendar.append(week_row)
+    return html.Table(calendar)
+
+# Callback for updating the calendar display
 @app.callback(
-    Output('schedule-output', 'children'),
+    Output('calendar-output', 'children'),
     [Input('month-selector', 'value'),
-     Input('start-btn', 'n_clicks')]
+     Input('start-btn', 'n_clicks'),
+     Input('reset-btn', 'n_clicks')]
 )
-def update_schedule(selected_month, n_clicks):
-    if n_clicks is not None:
-        scheduler.run(day_rate=5, days_to_run=30, month=selected_month)
-        # Convert the schedule data into a format suitable for display
-        schedule_data = scheduler.get_schedule_data()
-        return html.Table(
-            # Add table rows here based on the schedule_data
-        )
-    return "Select a month and start the simulation."
+def update_calendar(selected_month, start_clicks, reset_clicks):
+    ctx = dash.callback_context
+
+    if not ctx.triggered:
+        button_id = 'No clicks yet'
+    else:
+        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+
+    if button_id == 'start-btn' and start_clicks:
+        simulation.run_simulation(rate=5, run=30, month=selected_month)
+    elif button_id == 'reset-btn' and reset_clicks:
+        simulation.clear_simulation()
+
+    schedule_data = {} # Retrieve the actual schedule data here
+    return create_calendar(selected_month, schedule_data)
 
 # Run the Dash app
 if __name__ == '__main__':
