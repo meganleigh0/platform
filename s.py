80c418e9400ee1292377eb50c417e130fe17bb9a
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-14 17:59:44 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,31 +1,44 @@
+import dash
+from dash import dcc, html, dash_table
+from dash.dependencies import Input, Output, State
+import plotly.graph_objs as go
+import dash_bootstrap_components as dbc
+
+# Assuming the provided code, necessary libraries, and viz module are imported
+simulation = Simulation()
+
+# Select a Bootstrap theme
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# Function to generate the schedule table (implement according to your data structure)
 def generate_schedule_table(schedule):
-    # Placeholder function - replace with actual logic to generate a table from the schedule
-    table_header = [html.Thead(html.Tr([html.Th("Program"), html.Th("Quantity per Month")]))]
-    table_body = [html.Tr([html.Td(program), html.Td(quantity)]) for program, quantity in schedule.items()]
-    return html.Table(table_header + table_body)
-Step 2: Dash App Layout
-python
-Copy code
-app.layout = html.Div([
-    html.H1("Production Schedule Simulator"),
-    html.Div(id='schedule-table', children=generate_schedule_table(simulation.schedule.programs)),
-    dcc.Dropdown(
-        id='month-selector',
-        options=[{'label': month, 'value': month} for month in simulation.schedule.months],
-        value='January 2024'
-    ),
-    dcc.RadioItems(
-        id='duration-selector',
-        options=[{'label': 'Day', 'value': 1}, {'label': 'Week', 'value': 7}, {'label': 'Month', 'value': 30}],
-        value=1
-    ),
-    html.Button('Run Simulation', id='run-simulation-btn'),
-    html.Button('Clear Simulation', id='clear-simulation-btn'),
-    dcc.Graph(id='gantt-chart', figure=go.Figure())
-])
-Step 3: Callbacks for Simulation and Clearing
-python
-Copy code
+    # Convert schedule to DataFrame or similar structure if not already
+    return dash_table.DataTable(
+        data=schedule.to_dict('records'), 
+        columns=[{'name': i, 'id': i} for i in schedule.columns],
+        style_as_list_view=True,
+        style_header={'backgroundColor': 'rgb(30, 30, 30)', 'color': 'white'},
+        style_cell={'backgroundColor': 'rgb(50, 50, 50)', 'color': 'white'}
+    )
+
+app.layout = dbc.Container([
+    dbc.Row(dbc.Col(html.H1("Production Schedule Simulator"), width={'size': 6, 'offset': 3})),
+    dbc.Row(dbc.Col(html.Div(id='schedule-table'), width=12)),
+    dbc.Row([
+        dbc.Col(dcc.Dropdown(
+            id='month-selector',
+            options=[{'label': month, 'value': month} for month in simulation.schedule.months],
+            value='January 2024'), width=4),
+        dbc.Col(dcc.RadioItems(
+            id='duration-selector',
+            options=[{'label': 'Day', 'value': 1}, {'label': 'Week', 'value': 7}, {'label': 'Month', 'value': 30}],
+            value=1), width=4),
+        dbc.Col(dbc.Button('Run Simulation', id='run-simulation-btn', color="primary"), width=2),
+        dbc.Col(dbc.Button('Clear Simulation', id='clear-simulation-btn', color="secondary"), width=2)
+    ]),
+    dbc.Row(dbc.Col(dcc.Graph(id='gantt-chart', figure=go.Figure()), width=12))
+], fluid=True)
+
 @app.callback(
     [Output('gantt-chart', 'figure'),
      Output('schedule-table', 'children')],
@@ -35,21 +48,8 @@ Copy code
      State('duration-selector', 'value')]
 )
 def update_output(run_clicks, clear_clicks, selected_month, duration):
-    ctx = dash.callback_context
+    # ... (callback logic remains the same)
+    pass
 
-    if not ctx.triggered:
-        button_id = 'No clicks yet'
-    else:
-        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
-
-    if button_id == 'run-simulation-btn' and run_clicks:
-        simulation.run_simulation(rate, duration, selected_month)
-        gantt_chart = viz.gantt()  # Assuming this returns a figure object
-        return gantt_chart, generate_schedule_table(simulation.schedule.programs)
-
-    elif button_id == 'clear-simulation-btn' and clear_clicks:
-        # Clearing logic here
-        return go.Figure(), generate_schedule_table(simulation.schedule.programs)
-
-    return go.Figure(), generate_schedule_table(simulation.schedule.programs)
-This setup:
+if __name__ == '__main__':
+    app.run_server(debug=True)
