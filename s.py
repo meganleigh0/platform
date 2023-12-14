c522afc6cfc4064eba67caf6826d6de528c63bd9
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-13 22:31:59 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,91 +1,92 @@
-# Initialize simulation
-simulation = Simulation()
+import dash
+from dash import html, dcc, Input, Output, callback
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import pandas as pd
 
-# Function to convert schedule data to a format suitable for Dash DataTable
-def generate_table_data():
-    data = []
-    for (program, mbom), prog_obj in simulation.schedule.programs.items():
-        row = {'Program': program, 'MBOM': mbom}
-        for month in simulation.schedule.months:
-            row[month] = prog_obj.get_quantity_for_month(month)
-        data.append(row)
-    return data
+# Load your data
+df = pd.read_csv('simulation_data.csv')
+
+# Calculate duration for the scatter plot
+df['Duration'] = pd.to_datetime(df['Timestamp_end']) - pd.to_datetime(df['Timestamp_start'])
+df['Duration'] = df['Duration'].dt.total_seconds() / 3600  # Convert duration to hours
+
+# Initialize the Dash app with a Bootstrap theme
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # Define the layout of the app
-app.layout = html.Div([
-    # Manufacturing Simulation Dashboard
-    html.Div([
-        html.H1("Manufacturing Simulation Dashboard"),
-        html.H2("Program Schedule"),
-        dash_table.DataTable(
-            id='schedule-table',
-            editable=True,
-            columns=[{"name": "Program", "id": "Program"}, {"name": "MBOM", "id": "MBOM"}] +
-                    [{"name": month, "id": month} for month in simulation.schedule.months],
-            data=generate_table_data()
-        ),
-        html.Label('Rate', htmlFor='rate'),
-        dcc.Input(id='rate', type='number', placeholder="Enter Rate", value=1, min=1, max=50),
-        html.Label('Run Days', htmlFor='run'),
-        html.Button("Run Simulation", id='run-simulation-button', className='button')
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(html.H1("Manufacturing Simulation Dashboard", className="text-center mb-4"), width=12)
     ]),
+    dbc.Row([
+        dbc.Col([
+            dcc.Dropdown(
+                id='vehicle-dropdown',
+                options=[{'label': v, 'value': v} for v in df['Vehicle'].unique()],
+                value=df['Vehicle'].unique()[0],
+                clearable=False,
+                style={'width': '100%'}
+            )
+        ], width=4)
+    ]),
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='station-utilization-bar-chart'), width=6),
+        dbc.Col(dcc.Graph(id='assembly-duration-scatter-plot'), width=6)
+    ]),
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='workflow-time-series'), width=12)
+    ]),
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='data-table'), width=12)
+    ])
+], fluid=True)
 
-    # Additional Simulation Analysis (hidden initially)
-    dbc.Container([
-        dbc.Row([
-            dbc.Col(html.H1("Additional Simulation Analysis", className="text-center mb-4"), width=12)
-        ]),
-        dbc.Row([
-            dbc.Col([
-                dcc.Dropdown(
-                    id='vehicle-dropdown',
-                    options=[],  # Options will be set dynamically after simulation
-                    value=None,
-                    clearable=False,
-                    style={'width': '100%'}
-                )
-            ], width=4)
-        ]),
-        dbc.Row([
-            dbc.Col(dcc.Graph(id='station-utilization-bar-chart'), width=6),
-            dbc.Col(dcc.Graph(id='assembly-duration-scatter-plot'), width=6)
-        ]),
-        dbc.Row([
-            dbc.Col(dcc.Graph(id='workflow-time-series'), width=12)
-        ]),
-        dbc.Row([
-            dbc.Col(dcc.Graph(id='data-table'), width=12)
-        ])
-    ], fluid=True, style={'display': 'none'}, id='additional-analysis-container')
-], style={'width': '90%', 'margin': 'auto'})
-
-# Callback for running the simulation
+# Callback for the station utilization bar chart
 @app.callback(
-    Output('schedule-table', 'data'),
-    Output('vehicle-dropdown', 'options'),
-    Output('additional-analysis-container', 'style'),
-    [Input('run-simulation-button', 'n_clicks')],
-    [State('rate', 'value'), State('run', 'value')],
-    prevent_initial_call=True
+    Output('station-utilization-bar-chart', 'figure'),
+    Input('vehicle-dropdown', 'value')
 )
-def update_simulation_and_graphs(n_clicks, rate, run):
-    if n_clicks:
-        # Run the simulation
-        simulation.run_simulation(rate, run)
+def update_station_bar_chart(vehicle):
+    filtered_df = df[df['Vehicle'] == vehicle]
+    station_count = filtered_df['Station'].value_counts()
+    fig = px.bar(station_count, labels={'index': 'Station', 'value': 'Assembly Count'},
+                 template="plotly_dark")
+    fig.update_layout(transition_duration=500)
+    return fig
 
-        # Update the DataTable
-        new_data = generate_table_data()
+# Callback for the assembly duration scatter plot
+@app.callback(
+    Output('assembly-duration-scatter-plot', 'figure'),
+    Input('vehicle-dropdown', 'value')
+)
+def update_assembly_duration_scatter_plot(vehicle):
+    filtered_df = df[df['Vehicle'] == vehicle]
+    fig = px.scatter(filtered_df, x='Station', y='Duration', color='Assembly',
+                     template="plotly_dark")
+    fig.update_layout(transition_duration=500)
+    return fig
 
-        # Load and prepare new data for additional analysis
-        df = pd.read_csv('simulation_data.csv')
-        options = [{'label': v, 'value': v} for v in df['Vehicle'].unique()]
+# Callback for the workflow time series
+@app.callback(
+    Output('workflow-time-series', 'figure'),
+    Input('vehicle-dropdown', 'value')
+)
+def update_workflow_time_series(vehicle):
+    filtered_df = df[df['Vehicle'] == vehicle]
+    fig = px.line(filtered_df, x='Timestamp_start', y='Assembly', template="plotly_dark")
+    fig.update_layout(transition_duration=500)
+    return fig
 
-        return new_data, options, {'display': 'block'}
-
-    return no_update, no_update, no_update
-
-# Callbacks for additional graphs
-# (Implement the rest of the callbacks here, similar to the original callbacks in your second code snippet)
+# Callback for the data table
+@app.callback(
+    Output('data-table', 'figure'),
+    Input('vehicle-dropdown', 'value')
+)
+def update_data_table(vehicle):
+    filtered_df = df[df['Vehicle'] == vehicle]
+    fig = dbc.Table.from_dataframe(filtered_df, striped=True, bordered=True, hover=True)
+    return fig
 
 # Run the app
 if __name__ == '__main__':
