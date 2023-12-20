bc32eb93050d951aa421fc189b8d897bb95dee9a
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-20 17:50:11 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,6 +1,24 @@
-import pandas as pd
+def save_station_requirements_to_csv(schedule, filename='station_requirements.csv'):
+    data = []
 
-def save_schedule_requirements_to_csv(schedule, filename='schedule_requirements.csv'):
+    for program_key, program_obj in schedule.programs.items():
+        program_name = program_obj.name
+        for month in schedule.months:
+            quantity = program_obj.get_quantity_for_month(month)
+            if quantity > 0:
+                for station_name, station_hours in program_obj.product_station_req.items():
+                    data.append({
+                        'Program': program_name,
+                        'Month': month,
+                        'Station Name': station_name,
+                        'Station Hours': station_hours * quantity
+                    })
+
+    df = pd.DataFrame(data)
+    df.to_csv(filename, index=False)
+
+
+def save_department_requirements_to_csv(schedule, filename='department_requirements.csv'):
     data = []
 
     for program_key, program_obj in schedule.programs.items():
@@ -9,19 +27,15 @@ def save_schedule_requirements_to_csv(schedule, filename='schedule_requirements.
             quantity = program_obj.get_quantity_for_month(month)
             if quantity > 0:
                 for dept_id, dept_hours in program_obj.product_department_req.items():
-                    for station_name, station_hours in program_obj.product_station_req.items():
-                        data.append({
-                            'Program': program_name,
-                            'Month': month,
-                            'Department ID': dept_id,
-                            'Station Name': station_name,
-                            'Department Hours': dept_hours * quantity,
-                            'Station Hours': station_hours * quantity
-                        })
+                    data.append({
+                        'Program': program_name,
+                        'Month': month,
+                        'Department ID': dept_id,
+                        'Department Hours': dept_hours * quantity
+                    })
 
     df = pd.DataFrame(data)
     df.to_csv(filename, index=False)
 
 # Example usage
-# Assuming you have a 'schedule' object from your Schedule class
-save_schedule_requirements_to_csv(schedule)
+# save_department_requirements_to_csv(schedule)
