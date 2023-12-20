1c297fcf0cfee11b8367de39195e66bbf4eb2b8d
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-20 17:34:05 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import datetime
-import simpy  # Make sure simpy is installed
+import simpy  # Ensure simpy is installed
 from your_module import Scheduler, Schedule  # Replace with actual import
 
 # Initialize the Scheduler and Schedule
@@ -12,14 +12,17 @@ scheduler = Scheduler(env, schedule, datetime.date(2024, 1, 1))
 # Function to aggregate data for a selected month
 def aggregate_data_for_month(month):
     scheduler.set_month_requirements(month)
-    data = []
+    department_data = {}
+    station_data = {}
+
     for program_obj in schedule.programs.values():
-        data.append({
-            'Program': program_obj.name,
-            'Department Hours': program_obj.product_department_req,
-            'Station Hours': program_obj.product_station_req
-        })
-    return pd.DataFrame(data)
+        quantity = program_obj.get_quantity_for_month(month)
+        for dept_id, hours in program_obj.product_department_req.items():
+            department_data[dept_id] = department_data.get(dept_id, 0) + hours * quantity
+        for station_name, hours in program_obj.product_station_req.items():
+            station_data[station_name] = station_data.get(station_name, 0) + hours * quantity
+
+    return department_data, station_data
 
 # Streamlit layout
 st.title("Production Schedule Overview")
@@ -28,11 +31,26 @@ st.title("Production Schedule Overview")
 months = schedule.months  # Assuming this is a list of months
 selected_month = st.selectbox("Select a month", months)
 
-# Show aggregated data when a month is selected
+# Aggregated data display and visualization
 if selected_month:
-    aggregated_data = aggregate_data_for_month(selected_month)
-    st.header(f"Requirements for {selected_month}")
-    st.dataframe(aggregated_data)
+    department_hours, station_hours = aggregate_data_for_month(selected_month)
+    
+    # Display department data
+    st.header(f"Department Hours for {selected_month}")
+    st.dataframe(pd.DataFrame.from_dict(department_hours, orient='index', columns=['Hours']))
 
-# Additional Visualization (if required)
-# st.bar_chart(aggregated_data[['Department Hours', 'Station Hours']])
+    # Display station data
+    st.header(f"Station Hours for {selected_month}")
+    st.dataframe(pd.DataFrame.from_dict(station_hours, orient='index', columns=['Hours']))
+
+    # Visualizations
+    st.bar_chart(pd.DataFrame.from_dict(department_hours, orient='index', columns=['Hours']))
+    st.bar_chart(pd.DataFrame.from_dict(station_hours, orient='index', columns=['Hours']))
+
+    # Filtering options
+    st.sidebar.header("Filter Options")
+    # Example: Filter by Department
+    department_filter = st.sidebar.multiselect("Select Departments", list(department_hours.keys()))
+    if department_filter:
+        filtered_dept_hours = {dept: hours for dept, hours in department_hours.items() if dept in department_filter}
+        st.dataframe(pd.DataFrame.from_dict(filtered_dept_hours, orient='index', columns=['Hours']))
