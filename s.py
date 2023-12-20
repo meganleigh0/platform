77c81e205b3f819b7b0e74a7c3f59c8d4fd25407
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-20 15:11:26 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,36 +1,36 @@
-def generate_calendar_style_schedule_table(schedule, highlight_month):
-    data = []
-    for program_key, program_obj in schedule.programs.items():
-        program_name = program_key[0]
-        for month, quantity in program_obj.production_plan.items():
-            data.append({'Program': program_name, 'Month': month, 'Quantity': quantity})
-
-    df = pd.DataFrame(data)
-    month_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    pivot_df = df.pivot(index='Program', columns='Month', values='Quantity').fillna(0).reindex(columns=month_order).reset_index()
-    
-    return pivot_df
-Streamlit App with Month Selection and Highlighting
-python
-Copy code
 # Streamlit layout
 st.title("Production Schedule Simulator")
+
+simulation = Simulation()  # Create a simulation instance
 
 # Sidebar for user inputs
 with st.sidebar:
     st.header("Simulation Settings")
-    selected_month = st.selectbox("Select Month", options=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
-    # ... Other inputs like duration and rate
+    selected_month = st.selectbox("Select Month", options=simulation.schedule.months)
+    duration = st.radio("Select Duration", options=['Day', 'Week', 'Month'], index=0)
+    rate = st.number_input("Enter Rate", min_value=1, value=5)
+    
+    if st.button("Run Simulation"):
+        # Run simulation logic with progress bar
+        with st.spinner('Running Simulation...'):
+            progress_bar = st.progress(0)
+            for percent_complete in range(100):
+                time.sleep(0.1)  # Replace with actual simulation steps
+                progress_bar.progress(percent_complete + 1)
+            simulation.run_simulation(rate, {'Day': 1, 'Week': 7, 'Month': 30}[duration], selected_month)
+            gantt_chart = go.Figure()  # Replace with actual function to generate Gantt chart
+            st.session_state['gantt_chart'] = gantt_chart
+
+    if st.button("Clear Simulation"):
+        simulation.clear_simulation()
+        st.session_state['gantt_chart'] = go.Figure()
 
 # Main area
 st.header("Production Schedule")
 schedule_data = generate_calendar_style_schedule_table(simulation.schedule, selected_month)
+st.dataframe(schedule_data)
 
-# Apply the highlighting style for the selected month
-def highlight_month(df, month):
-    return [
-        'background-color: #ff7f0e' if col == month else '' 
-        for col in df.columns
-    ]
-
-st.dataframe(schedule_data.style.apply(highlight_month, month=selected_month))
+# Display Gantt Chart
+st.header("Gantt Chart Visualization")
+if 'gantt_chart' in st.session_state:
+    st.plotly_chart(st.session_state['gantt_chart'])
