8182a366e4f44b2b80d32723f73f5fde70df2ef4
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-20 17:23:17 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,29 +1,34 @@
 import streamlit as st
 import pandas as pd
-from your_module import Simulation, Scheduler, Program
+import datetime
+import simpy  # Make sure to import simpy or the required environment library
+from your_module import Simulation  # Replace 'your_module' with your actual module name
 
 # Initialize the Simulation
 simulation = Simulation()
 
-# Function to aggregate required hours by department and station
+# Function to simulate product generation and aggregate data
 def aggregate_data(simulation):
-    data = {
-        'program': [],
-        'month': [],
-        'hours_by_department': [],
-        'hours_by_station': []
-    }
+    aggregated_data = []
 
-    for (program_key, program) in simulation.schedule.programs.items():
+    for program_key, program in simulation.schedule.programs.items():
         for month in simulation.schedule.months:
             quantity = program.get_quantity_for_month(month)
             if quantity > 0:
-                data['program'].append(program.name)
-                data['month'].append(month)
-                data['hours_by_department'].append(program.product_department_req * quantity)
-                data['hours_by_station'].append(program.product_station_req * quantity)
+                # Simulate product generation
+                for _ in range(quantity):
+                    product = program.gen_product(..., simulation.scheduler.station_dict, simulation.scheduler.department_dict)  # Fill in the missing arguments
+                    # Aggregate data
+                    department_hours = program.product_department_req  # Example, replace with actual calculation
+                    station_hours = program.product_station_req  # Example, replace with actual calculation
+                    aggregated_data.append({
+                        'Program': program.name,
+                        'Month': month,
+                        'Department Hours': department_hours,
+                        'Station Hours': station_hours
+                    })
 
-    return pd.DataFrame(data)
+    return pd.DataFrame(aggregated_data)
 
 # Aggregate the data
 aggregated_data = aggregate_data(simulation)
@@ -32,8 +37,8 @@ aggregated_data = aggregate_data(simulation)
 st.title("Production Schedule Overview")
 
 # Display the aggregated data in a table
-st.header("Program Requirements by Month")
+st.header("Aggregated Program Requirements by Month")
 st.dataframe(aggregated_data)
 
 # Additional Visualization (if required)
-# st.bar_chart(aggregated_data['hours_by_department'])
+# st.bar_chart(aggregated_data[['Department Hours', 'Station Hours']])
