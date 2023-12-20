434ebf265dee3ed36aa7c5b1d9b67c1a6778aaa3
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-20 17:38:18 -0500

    Update s.py

--- s.py
+++ s.py
@@ -9,20 +9,31 @@ env = simpy.Environment()  # Replace with actual environment setup
 schedule = Schedule(env)  # Replace with actual schedule loading method
 scheduler = Scheduler(env, schedule, datetime.date(2024, 1, 1))
 
-# Function to aggregate data for a selected month
-def aggregate_data_for_month(month):
+# Function to aggregate data for a selected month and filter by program
+def aggregate_data_for_month(month, program_filter=None, vehicle_filter=None):
     scheduler.set_month_requirements(month)
-    department_data = {}
-    station_data = {}
+    data = []
 
-    for program_obj in schedule.programs.values():
+    for program_key, program_obj in schedule.programs.items():
+        if program_filter and program_obj.name not in program_filter:
+            continue
+
         quantity = program_obj.get_quantity_for_month(month)
-        for dept_id, hours in program_obj.product_department_req.items():
-            department_data[dept_id] = department_data.get(dept_id, 0) + hours * quantity
-        for station_name, hours in program_obj.product_station_req.items():
-            station_data[station_name] = station_data.get(station_name, 0) + hours * quantity
+        if quantity > 0:
+            for dept_id, hours in program_obj.product_department_req.items():
+                for station_name, hours_station in program_obj.product_station_req.items():
+                    data.append({
+                        'Program': program_obj.name,
+                        'Department': dept_id,
+                        'Station': station_name,
+                        'Department Hours': hours * quantity,
+                        'Station Hours': hours_station * quantity
+                    })
 
-    return department_data, station_data
+    df = pd.DataFrame(data)
+    if vehicle_filter:
+        df = df[df['Vehicle'].isin(vehicle_filter)]
+    return df
 
 # Streamlit layout
 st.title("Production Schedule Overview")
@@ -31,26 +42,21 @@ st.title("Production Schedule Overview")
 months = schedule.months  # Assuming this is a list of months
 selected_month = st.selectbox("Select a month", months)
 
-# Aggregated data display and visualization
+# Filters
+st.sidebar.header("Filter Options")
+program_filter = st.sidebar.multiselect("Select Programs", [p.name for p in schedule.programs.values()])
+vehicle_filter = st.sidebar.multiselect("Select Vehicles", [v.name for v in schedule.vehicles.values()])  # Assuming you have a list of vehicles
+
+# Display and visualize data
 if selected_month:
-    department_hours, station_hours = aggregate_data_for_month(selected_month)
+    aggregated_data = aggregate_data_for_month(selected_month, program_filter, vehicle_filter)
     
-    # Display department data
+    # Visualizations - Department Hours
     st.header(f"Department Hours for {selected_month}")
-    st.dataframe(pd.DataFrame.from_dict(department_hours, orient='index', columns=['Hours']))
+    department_chart_data = aggregated_data.groupby('Department')['Department Hours'].sum()
+    st.bar_chart(department_chart_data)
 
-    # Display station data
+    # Visualizations - Station Hours
     st.header(f"Station Hours for {selected_month}")
-    st.dataframe(pd.DataFrame.from_dict(station_hours, orient='index', columns=['Hours']))
-
-    # Visualizations
-    st.bar_chart(pd.DataFrame.from_dict(department_hours, orient='index', columns=['Hours']))
-    st.bar_chart(pd.DataFrame.from_dict(station_hours, orient='index', columns=['Hours']))
-
-    # Filtering options
-    st.sidebar.header("Filter Options")
-    # Example: Filter by Department
-    department_filter = st.sidebar.multiselect("Select Departments", list(department_hours.keys()))
-    if department_filter:
-        filtered_dept_hours = {dept: hours for dept, hours in department_hours.items() if dept in department_filter}
-        st.dataframe(pd.DataFrame.from_dict(filtered_dept_hours, orient='index', columns=['Hours']))
+    station_chart_data = aggregated_data.groupby('Station')['Station Hours'].sum()
+    st.bar_chart(station_chart_data)
