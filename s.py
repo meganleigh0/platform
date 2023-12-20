adef73197fa7b81e17afc5611a6bfe1b73a1eb09
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-20 15:32:42 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,21 +1,48 @@
-def generate_calendar_style_schedule_table(schedule, selected_month):
-    data = []
-    for program_key, program_obj in schedule.programs.items():
-        program_name = program_key[0]
-        for month, quantity in program_obj.production_plan.items():
-            data.append({'Program': program_name, 'Month': month, 'Quantity': quantity})
+import plotly.graph_objects as go
+import plotly.subplots as sp
+import pandas as pd
 
-    df = pd.DataFrame(data)
-    month_order = schedule.months
-    pivot_df = df.pivot(index='Program', columns='Month', values='Quantity').fillna(0).reindex(columns=month_order).reset_index()
+# Assuming df is your DataFrame
+# df = pd.read_csv('your_data.csv')  # Load your DataFrame
 
-    # Function to apply the highlighting style
-    def highlight_selected_month(s):
-        if s.name == selected_month:
-            return ['background-color: #ff7f0e' for _ in s]
-        else:
-            return ['' for _ in s]
+# Aggregate utilization by plant
+plant_utilization = df.groupby('Plant')['Utilization'].mean().reset_index()
 
-    # Apply the styling to the DataFrame
-    styled_df = pivot_df.style.apply(highlight_selected_month)
-    return styled_df
+# Create a subplot
+fig = sp.make_subplots(
+    rows=2, cols=len(plant_utilization['Plant'].unique()),
+    subplot_titles=plant_utilization['Plant'].unique(),
+    specs=[[{"type": "indicator"}] * len(plant_utilization), [{"type": "bar"}] * len(plant_utilization)]
+)
+
+# Add gauges for each plant
+for i, plant in enumerate(plant_utilization['Plant'].unique(), 1):
+    fig.add_trace(
+        go.Indicator(
+            mode="gauge+number",
+            value=plant_utilization[plant_utilization['Plant'] == plant]['Utilization'].iloc[0],
+            title={'text': f"Plant {plant}"},
+            gauge={'axis': {'range': [0, 100]}, 'bar': {'color': "darkblue"}}
+        ),
+        row=1, col=i
+    )
+
+    # Add detailed bar chart for each plant by department
+    department_utilization = df[df['Plant'] == plant].groupby('Department')['Utilization'].mean().reset_index()
+    fig.add_trace(
+        go.Bar(
+            x=department_utilization['Department'],
+            y=department_utilization['Utilization'],
+            name=f"Dept Utilization in {plant}"
+        ),
+        row=2, col=i
+    )
+
+# Update layout
+fig.update_layout(
+    title_text="Plant and Department Utilization Overview",
+    height=600,
+    showlegend=False
+)
+
+fig.show()
