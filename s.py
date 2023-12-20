bb916991054d3bd92432c778f8411f7213c24440
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-20 15:57:13 -0500

    Update s.py

--- s.py
+++ s.py
@@ -2,53 +2,67 @@ import plotly.graph_objects as go
 import plotly.subplots as sp
 import pandas as pd
 
-# Assuming df is your DataFrame
+def create_utilization_dashboard(df):
+    """
+    Create a dashboard showing plant and department utilization.
 
-# Convert utilization to percentage
-df['Utilization'] = df['Utilization'] * 100
+    Parameters:
+    df (DataFrame): A DataFrame containing columns - Department, DepartmentID, Plant, Timestamp, Utilization
 
-# Aggregate utilization by plant
-plant_utilization = df.groupby('plant')['Utilization'].mean().reset_index()
+    Returns:
+    go.Figure: A Plotly figure object containing the visualization.
+    """
 
-# Number of unique plants
-num_plants = len(plant_utilization['plant'].unique())
+    # Convert utilization to percentage for easier interpretation
+    df['Utilization'] = df['Utilization'] * 100
 
-# Create a subplot
-fig = sp.make_subplots(
-    rows=2, cols=num_plants,
-    subplot_titles=[f"Plant {plant}" for plant in plant_utilization['plant'].unique()] + [None] * num_plants,
-    specs=[[{"type": "indicator"}] * num_plants, [{"type": "bar"}] * num_plants]
-)
+    # Aggregate utilization by plant to get an average utilization for each plant
+    plant_utilization = df.groupby('plant')['Utilization'].mean().reset_index()
 
-# Add gauges and bars for each plant
-for i, plant in enumerate(plant_utilization['plant'].unique(), 1):
-    # Gauge chart for plant utilization
-    fig.add_trace(
-        go.Indicator(
-            mode="gauge+number",
-            value=plant_utilization[plant_utilization['plant'] == plant]['Utilization'].iloc[0],
-            title={'text': f"Plant {plant} Utilization"},
-            gauge={'axis': {'range': [0, 100]}, 'bar': {'color': "darkblue"}}
-        ),
-        row=1, col=i
+    # Number of unique plants determines the number of columns in the subplot
+    num_plants = len(plant_utilization['plant'].unique())
+
+    # Create a subplot with two rows: one for plant gauges and one for department bar charts
+    fig = sp.make_subplots(
+        rows=2, cols=num_plants,
+        subplot_titles=[f"Plant {plant}" for plant in plant_utilization['plant'].unique()] + [None] * num_plants,
+        specs=[[{"type": "indicator"}] * num_plants, [{"type": "bar"}] * num_plants]
     )
 
-    # Bar chart for department utilization within the plant
-    department_utilization = df[df['plant'] == plant].groupby('Department')['Utilization'].mean().reset_index()
-    fig.add_trace(
-        go.Bar(
-            x=department_utilization['Department'],
-            y=department_utilization['Utilization'],
-            name=f"Dept Utilization in {plant}"
-        ),
-        row=2, col=i
+    # Add gauge and bar charts for each plant
+    for i, plant in enumerate(plant_utilization['plant'].unique(), 1):
+        # Gauge chart for overall plant utilization
+        fig.add_trace(
+            go.Indicator(
+                mode="gauge+number",
+                value=plant_utilization[plant_utilization['plant'] == plant]['Utilization'].iloc[0],
+                title={'text': f"Plant {plant} Utilization"},
+                gauge={'axis': {'range': [0, 100]}, 'bar': {'color': "darkblue"}}
+            ),
+            row=1, col=i
+        )
+
+        # Bar chart for detailed department utilization within the plant
+        department_utilization = df[df['plant'] == plant].groupby('Department')['Utilization'].mean().reset_index()
+        fig.add_trace(
+            go.Bar(
+                x=department_utilization['Department'],
+                y=department_utilization['Utilization'],
+                name=f"Dept Utilization in {plant}"
+            ),
+            row=2, col=i
+        )
+
+    # Update layout for better presentation
+    fig.update_layout(
+        title_text="Plant and Department Utilization Overview",
+        height=600,
+        showlegend=False
     )
 
-# Update layout
-fig.update_layout(
-    title_text="Plant and Department Utilization Overview",
-    height=600,
-    showlegend=False
-)
+    return fig
 
-fig.show()
+# Example usage:
+# df = pd.read_csv('your_data.csv')  # Load your DataFrame
+# fig = create_utilization_dashboard(df)
+# fig.show()
