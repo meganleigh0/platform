287f7838e42cb063137310cb491104e211b70254
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-20 14:49:58 -0500

    Update s.py

--- s.py
+++ s.py
@@ -2,33 +2,50 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objs as go
 import simpy  # Ensure simpy is installed
+import time
 
 # Import or define your Simulation, Schedule, and other classes here
 
 simulation = Simulation()  # Create a simulation instance
 
 def generate_calendar_style_schedule_table(schedule):
-    # Your existing function code here
+    # Your existing function code
 
 # Streamlit layout
 st.title("Production Schedule Simulator")
 
-# Sidebar
+# Sidebar for user inputs
 with st.sidebar:
+    st.header("Simulation Settings")
     selected_month = st.selectbox("Select Month", options=simulation.schedule.months)
     duration = st.radio("Select Duration", options=['Day', 'Week', 'Month'], index=0)
     rate = st.number_input("Enter Rate", min_value=1, value=5)
+    
     if st.button("Run Simulation"):
-        # Run simulation logic
+        # Run simulation logic with progress bar
+        with st.spinner('Running Simulation...'):
+            progress_bar = st.progress(0)
+            for percent_complete in range(100):
+                time.sleep(0.1)  # Replace with actual simulation steps
+                progress_bar.progress(percent_complete + 1)
+            simulation.run_simulation(rate, {'Day': 1, 'Week': 7, 'Month': 30}[duration], selected_month)
+            gantt_chart = viz.gantt()  # Replace with actual function to generate Gantt chart
+            st.session_state['gantt_chart'] = gantt_chart
+
     if st.button("Clear Simulation"):
-        # Clear simulation logic
+        simulation.clear_simulation()
+        st.session_state['gantt_chart'] = go.Figure()
 
 # Main area
+st.header("Production Schedule")
 schedule_data = generate_calendar_style_schedule_table(simulation.schedule)
-st.dataframe(schedule_data.style.applymap(lambda x: 'background-color: lightblue'))
 
-# Gantt Chart
+# Displaying the schedule in a calendar-like format
+st.dataframe(schedule_data.style.apply(lambda x: ['background-color: lightgreen' if v != 0 else '' for v in x], axis=1))
+
+# Display Gantt Chart
+st.header("Gantt Chart Visualization")
 if 'gantt_chart' in st.session_state:
-    st.plotly_chart(st.session_state.gantt_chart)
+    st.plotly_chart(st.session_state['gantt_chart'])
 
-# Additional enhancements can be added as per the above suggestions
+# Additional enhancements and functionalities can be added as needed
