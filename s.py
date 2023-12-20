144e0c5028c7872bc013c62c23ea31c525d56ca8
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-20 17:26:54 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,44 +1,38 @@
 import streamlit as st
 import pandas as pd
 import datetime
-import simpy  # Make sure to import simpy or the required environment library
-from your_module import Simulation  # Replace 'your_module' with your actual module name
+import simpy  # Make sure simpy is installed
+from your_module import Scheduler, Schedule  # Replace with actual import
 
-# Initialize the Simulation
-simulation = Simulation()
+# Initialize the Scheduler and Schedule
+env = simpy.Environment()  # Replace with actual environment setup
+schedule = Schedule(env)  # Replace with actual schedule loading method
+scheduler = Scheduler(env, schedule, datetime.date(2024, 1, 1))
 
-# Function to simulate product generation and aggregate data
-def aggregate_data(simulation):
-    aggregated_data = []
-
-    for program_key, program in simulation.schedule.programs.items():
-        for month in simulation.schedule.months:
-            quantity = program.get_quantity_for_month(month)
-            if quantity > 0:
-                # Simulate product generation
-                for _ in range(quantity):
-                    product = program.gen_product(..., simulation.scheduler.station_dict, simulation.scheduler.department_dict)  # Fill in the missing arguments
-                    # Aggregate data
-                    department_hours = program.product_department_req  # Example, replace with actual calculation
-                    station_hours = program.product_station_req  # Example, replace with actual calculation
-                    aggregated_data.append({
-                        'Program': program.name,
-                        'Month': month,
-                        'Department Hours': department_hours,
-                        'Station Hours': station_hours
-                    })
-
-    return pd.DataFrame(aggregated_data)
-
-# Aggregate the data
-aggregated_data = aggregate_data(simulation)
+# Function to aggregate data for a selected month
+def aggregate_data_for_month(month):
+    scheduler.set_month_requirements(month)
+    data = []
+    for program_obj in schedule.programs.values():
+        data.append({
+            'Program': program_obj.name,
+            'Department Hours': program_obj.product_department_req,
+            'Station Hours': program_obj.product_station_req
+        })
+    return pd.DataFrame(data)
 
 # Streamlit layout
 st.title("Production Schedule Overview")
 
-# Display the aggregated data in a table
-st.header("Aggregated Program Requirements by Month")
-st.dataframe(aggregated_data)
+# Display months in the schedule
+months = schedule.months  # Assuming this is a list of months
+selected_month = st.selectbox("Select a month", months)
+
+# Show aggregated data when a month is selected
+if selected_month:
+    aggregated_data = aggregate_data_for_month(selected_month)
+    st.header(f"Requirements for {selected_month}")
+    st.dataframe(aggregated_data)
 
 # Additional Visualization (if required)
 # st.bar_chart(aggregated_data[['Department Hours', 'Station Hours']])
