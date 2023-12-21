dc7cf8503dde2f7745c69b76ae8de75c67b6adcd
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-20 19:01:00 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,10 +1,11 @@
+# Hardcoded month order
+month_order = ["January 2024", "February 2024", "March 2024", "April 2024", "May 2024", 
+               "June 2024", "July 2024", "August 2024", "September 2024", "October 2024", 
+               "November 2024", "December 2024"]
+
 # Function to sort months
 def sort_months(months):
-    month_order = ["January 2024", "February 2024", "March 2024", "April 2024", "May 2024", 
-                   "June 2024", "July 2024", "August 2024", "September 2024", "October 2024", 
-                   "November 2024", "December 2024"]
-    sorted_months = [month for month in month_order if month in months]
-    return sorted_months
+    return [month for month in month_order if month in months]
 
 # Function to calculate hours
 def calculate_hours(requirements_df, schedule_df, selected_month=None, selected_program=None):
@@ -13,6 +14,7 @@ def calculate_hours(requirements_df, schedule_df, selected_month=None, selected_
     if selected_month:
         filtered_schedule = filtered_schedule[['Program', selected_month]]
     else:
+        # Order columns according to sorted months
         filtered_schedule = filtered_schedule[['Program'] + sort_months(list(schedule_df.columns[2:]))]
 
     if selected_program and selected_program != 'All Programs':
@@ -36,7 +38,7 @@ def calculate_hours(requirements_df, schedule_df, selected_month=None, selected_
 
 # Streamlit layout
 st.set_page_config(layout="wide")
-st.title("Production Schedule Analysis", anchor=None)
+st.title("Production Schedule Analysis")
 
 # Selection options
 months = ['All Months'] + sort_months(list(schedule.columns[2:]))
@@ -49,17 +51,18 @@ station_hours, station_vehicle_hours, station_program_hours = calculate_hours(st
 department_hours, department_vehicle_hours, department_program_hours = calculate_hours(department_requirements, schedule, selected_month if selected_month != 'All Months' else None, selected_program)
 
 # Display the schedule for context
-st.header("Production Schedule", anchor=None)
+st.header("Production Schedule")
 st.dataframe(schedule)
 
 # Display results
-st.header("Station and Department Hours", anchor=None)
+st.header("Station and Department Hours")
 col1, col2 = st.columns(2)
 with col1:
     st.subheader("Station Hours")
     if selected_month != 'All Months':
         st.bar_chart(station_hours.groupby('Station')['Total Hours'].sum())
     else:
+        # Plot line chart with sorted months
         st.line_chart(station_hours.groupby('Station')[sort_months(schedule.columns[2:])].sum())
 
 with col2:
@@ -67,9 +70,5 @@ with col2:
     if selected_month != 'All Months':
         st.bar_chart(department_hours.groupby('DepartmentID')['Total Hours'].sum())
     else:
+        # Plot line chart with sorted months
         st.line_chart(department_hours.groupby('DepartmentID')[sort_months(schedule.columns[2:])].sum())
-
-# Display total hours for a vehicle and the program
-if selected_month != 'All Months' and selected_program != 'All Programs':
-    st.write(f"Total Hours for One Vehicle in {selected_program}: {station_vehicle_hours} (Station), {department_vehicle_hours} (Department)")
-    st.write(f"Total Hours for Program {selected_program} in {selected_month}: {station_program_hours} (Station), {department_program_hours} (Department)")
