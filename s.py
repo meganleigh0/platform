9018b2eb582f242994bb3d440641a427f0f0b155
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2023-12-20 19:36:40 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,33 +1,34 @@
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-
 # Load data
 df = pd.read_csv('logs/assembly.csv')
 df['Duration'] = round(df['Timestamp_end'] - df['Timestamp_start'], 2)
 
-st.title("Simulation Vehicle Data")
+# Page title
+st.title("Vehicle Assembly Data Visualization")
 
 # Sidebar for vehicle selection
-st.sidebar.header("Vehicle Information")
-vehicle = st.sidebar.selectbox("Select Vehicle", df['Vehicle'].unique())
+st.sidebar.header("Vehicle Selection")
+vehicle = st.sidebar.selectbox("Choose a Vehicle", df['Vehicle'].unique())
 filtered_df = df[df['Vehicle'] == vehicle]
 
 # Station Utilization Bar Chart
+st.subheader("Station Utilization Overview")
 station_count = filtered_df['Station'].value_counts()
 fig1 = px.bar(station_count, labels={'index': 'Station', 'value': 'Assembly Count'})
 fig1.update_layout(title='Station Assembly Count', yaxis_title='Assembly Count', width=1000)
 st.plotly_chart(fig1)
 
 # Assembly Duration Scatter Plot
+st.subheader("Assembly Duration Analysis")
 fig2 = px.scatter(filtered_df, x='Station', y='Duration', color='Assembly')
 fig2.update_layout(title='Assembly Duration per Station', width=1000)
 st.plotly_chart(fig2)
 
 # Workflow Time Series
+st.subheader("Workflow Time Series")
 fig3 = px.line(filtered_df, x='Timestamp_start', y='Assembly')
 fig3.update_layout(title='Workflow Time Series', width=1000)
 st.plotly_chart(fig3)
 
 # Data Table
+st.subheader("Detailed Data View")
 st.write(filtered_df)
